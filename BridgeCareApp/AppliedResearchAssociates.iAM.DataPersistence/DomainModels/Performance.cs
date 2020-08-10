f9133b701ae291692238cad750d1414b9a02b21b
Saransh Bhardwaj <saranshbhardwaj@gmail.com>
2020-08-10 16:33:45 -0500

    added first cut of classes

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistence/DomainModels/Performance.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppliedResearchAssociates.iAM.DataPersistence.DomainModels
+{
+    public class Performance
+    {
+    }
+}
---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistence/Repositories/InventoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AppliedResearchAssociates.iAM.DataPersistence.DomainModels;
+
+namespace AppliedResearchAssociates.iAM.DataPersistence.Repositories
+{
+    public class InventoryRepository : GenericMSSQLRepository<Inventory>
+    {
+        public InventoryRepository(BridgeCareContext context) : base(context)
+        {
+
+        }
+    }
+
+    public class BridgeCareContext
+    {
+    }
+}

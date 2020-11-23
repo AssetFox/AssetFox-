4f293d1a60c2f7b4bf408a25aacd5b1ffb5c681e
sbhardwaj <sbhardwaj@ara.com>
2020-11-23 17:21:36 -0600

    removed unused properties

--- BridgeCareApp/BridgeCareCore/Models/SummaryReport/WorkSummaryModel.cs
+++ BridgeCareApp/BridgeCareCore/Models/SummaryReport/WorkSummaryModel.cs
@@ -9,16 +9,5 @@ namespace BridgeCareCore.Models.SummaryReport
     {
         public List<double> PreviousYearInitialMinC { get; set; }
         public Dictionary<int, (int on, int off)> PoorOnOffCount { get; set; }
-        //public List<SimulationDataModel> SimulationDataModels { get; set; }
-
-        //public List<BridgeDataModel> BridgeDataModels { get; set; }
-
-        //public List<string> Treatments { get; set; }
-
-        //public List<BudgetsPerBRKey> BudgetsPerBRKeys { get; set; }
-
-        //public List<UnfundedRecommendationModel> UnfundedRecommendations { get; set; }
-
-        //public ParametersModel ParametersModel { get; set; }
     }
 }

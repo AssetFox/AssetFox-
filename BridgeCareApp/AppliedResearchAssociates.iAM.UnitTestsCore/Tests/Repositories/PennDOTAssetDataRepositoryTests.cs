ff4e7a89877856f483396c42a51757ce776fa4b8
Jacob Walter <jwalter@ara.com>
2021-03-25 17:14:48 -0400

    Added testing framework and test data for PennDOT Asset Data Repo

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/Repositories/PennDOTAssetDataRepositoryTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Moq;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL;
+using AppliedResearchAssociates.iAM.UnitTestsCore.TestData;
+
+namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests.Repositories
+{
+    public class PennDOTAssetDataRepositoryTests
+    {
+
+        [Fact]
+        public void GeneratesKeyPropertiesDictionaryWithNumericKey()
+        {
+
+        }
+
+        [Fact]
+        public void GeneratesKeyPropertiesDictionaryWithStringKey()
+        {
+
+        }
+
+        [Fact]
+        public void HandlesMissingKeyInAttributeRepository()
+        {
+
+        }
+
+        [Fact]
+        public void HandlesNoDataForExistingAttribute()
+        {
+
+        }
+
+        [Fact]
+        public void ReturnsSegmeentData()
+        {
+
+        }
+
+        [Fact]
+        public void HandlesUnmatchedKey()
+        {
+
+        }
+
+        [Fact]
+        public void HandlesNoSegmentFound()
+        {
+            // Should the system also remove the asset from KeyProperties if not found?  I think so.
+        }
+
+        [Fact]
+        public void HandlesOnlyTextAttributes()
+        {
+
+        }
+
+        [Fact]
+        public void HandlesOnlyNumericAttributes()
+        {
+
+        }
+    }
+}

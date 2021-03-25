421f974c9ba96941728901351a747bc1a1495288
ppatorno <ppatorno@ara.com>
2021-03-25 14:20:57 -0500

    -added a HubControllerBase class as a way of improving the sending of hub messages to the frontend (controller can be extended to existing controllers and used in try/catch blocks to send back meaningful error messages) -added a MockLegacySimulationSynchronizerService class in order to adjust test data (tests were taking too long to execute so added som functions to reduce some pieces of test data to get test to run faster) -updated migration with schema changes

---
+++ BridgeCareApp/BridgeCareCore/Controllers/HubControllerBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BridgeCareCore.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BridgeCareCore.Controllers
+{
+    public class HubControllerBase : ControllerBase
+    {
+        private readonly IHubContext<BridgeCareHub> _hubContext;
+
+        public HubControllerBase(IHubContext<BridgeCareHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public void SendRealTimeMessage(string method, object arg) =>
+            _hubContext?.Clients?.All?.SendAsync(method, arg);
+    }
+}

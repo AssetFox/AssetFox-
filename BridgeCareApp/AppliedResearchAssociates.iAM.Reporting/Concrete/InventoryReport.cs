7d544d00880ff767e5c77553c3893ef7e4b3c2fb
Jacob Walter <jwalter@ara.com>
2021-03-26 09:55:28 -0400

    Inventory report produces something

--- BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Concrete/InventoryReport.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Concrete/InventoryReport.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
 using Newtonsoft.Json;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories;
+using System.Text;
 
 namespace AppliedResearchAssociates.iAM.Reporting
 {
@@ -38,7 +40,38 @@ namespace AppliedResearchAssociates.iAM.Reporting
             _networkId = _repository.NetworkRepo.GetPennDotNetwork().Id;
         }
 
-        public Task Run(string parameters) => throw new NotImplementedException();
+        public async Task Run(string parameters)
+        {
+            var reportKeys = Parse(parameters);
+            if (reportKeys.BRKey == -1) return; // report failed due to bad parameters
+            if (!Validate(reportKeys)) return; // report failed due to validation
+
+            // Check if asset actually exists
+            List<SegmentAttributeDatum> segmentData;
+            string providedKey;
+            if (reportKeys.BRKey < 1)
+            {
+                providedKey = $"BMSID: {reportKeys.BMSID}";
+                segmentData = _repository.AssetDataRepository.GetAssetAttributes("BMSID", reportKeys.BMSID);
+            }
+            else
+            {
+                providedKey = $"BRKey:  {reportKeys.BRKey}";
+                segmentData = _repository.AssetDataRepository.GetAssetAttributes("BRKey", reportKeys.BRKey.ToString());
+            }
+            if (segmentData.Count() < 1)
+            {
+                // No data returned for key
+                Errors.Add($"Provided key {providedKey} not found");
+                return;
+            }
+
+            var resultsString = new StringBuilder();
+            resultsString.Append($"<p>BRKey: {segmentData.First(_ => _.Name == "BRKey").Value}");
+            resultsString.Append($"<p>BMSID: {segmentData.First(_ => _.Name == "BMSID").Value}");
+            Results = resultsString.ToString();
+            return;
+        }
 
         private InventoryParameters Parse(string parameters)
         {
@@ -59,21 +92,42 @@ namespace AppliedResearchAssociates.iAM.Reporting
             }
         }
 
-        //private bool Validate(InventoryParameters parameters)
-        //{
-        //    if (parameters.BRKey < 1 && String.IsNullOrEmpty(parameters.BMSID))
-        //    {
-        //        // No parameters provided
-        //        return false;
-        //    }
+        private bool Validate(InventoryParameters parameters)
+        {
+            if (parameters.BRKey < 1 && String.IsNullOrEmpty(parameters.BMSID))
+            {
+                // No parameters provided
+                return false;
+            }
 
-        //    if (parameters.BRKey > 0 && !String.IsNullOrEmpty(parameters.BMSID))
-        //    {
-        //        // Both parameters provided.  Check to see if they are the same asset
-        //        _repository.MaintainableAssetRepo.GetAllInNetworkWithAssignedDataAndLocations(_networkId).Where(_ => _.)
-        //    }
+            if (parameters.BRKey > 0 && !String.IsNullOrEmpty(parameters.BMSID))
+            {
+                // Both parameters provided.  Check to see if they are the same asset
+                var BRKeyGuid = _repository.AssetDataRepository.KeyProperties["BRKey"].FirstOrDefault(_ => _.KeyValue.Value == parameters.BRKey.ToString());
+                if (BRKeyGuid == null)
+                {
+                    // BRKey was not found
+                    Errors.Add($"Unable to find BRKey {parameters.BRKey}.  Did not attempt to find {parameters.BMSID}");
+                    return false;
+                }
+                var BMSIDGuid = _repository.AssetDataRepository.KeyProperties["BMSID"].FirstOrDefault(_ => _.KeyValue.Value == parameters.BMSID);
+                if (BMSIDGuid == null)
+                {
+                    // BMSID was not found
+                    Errors.Add($"Unable to find BMSID {parameters.BMSID}.  Will not use {parameters.BRKey}");
+                    return false;
+                }
+                if (BRKeyGuid.SegmentId != BMSIDGuid.SegmentId)
+                {
+                    // Keys were provided for two different assets
+                    Errors.Add($"The BRKey {parameters.BRKey} and BMSID {parameters.BMSID}.  No report will be provided");
+                    return false;
+                }
+            }
 
-        //    // Check if asset actually exists
-        //}
+            // All checks passed
+            return true;
+        }
+
     }
 }

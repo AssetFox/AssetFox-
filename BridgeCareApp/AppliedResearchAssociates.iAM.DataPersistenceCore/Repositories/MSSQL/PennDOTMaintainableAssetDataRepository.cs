207e76ea59fe550a0c2b6497b54566234e2c0c21
Jacob Walter <jwalter@ara.com>
2021-03-30 17:31:50 -0400

    Implement IAssetData for new schema and test

--- BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/PennDOTMaintainableAssetDataRepository.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/PennDOTMaintainableAssetDataRepository.cs
@@ -24,14 +24,98 @@ namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL
                 brkeyDatum.Add(new KeySegmentDatum()
                 {
                     SegmentId = asset.Id,
-                    //KeyValue = new SegmentAttributeDatum("BRKEY_", asset.Fac)
+                    KeyValue = new SegmentAttributeDatum("BRKEY", asset.FacilityName)
+                });
+                bmsidDatum.Add(new KeySegmentDatum()
+                {
+                    SegmentId = asset.Id,
+                    KeyValue = new SegmentAttributeDatum("BMSID", asset.SectionName)
                 });
             }
+
+            KeyProperties.Add("BRKEY", brkeyDatum);
+            KeyProperties.Add("BMSID", bmsidDatum);
         }
 
         public Dictionary<string, List<KeySegmentDatum>> KeyProperties { get; private set; }
 
-        public List<SegmentAttributeDatum> GetAssetAttributes(string keyName, string keyValue) => throw new NotImplementedException();
-        public Dictionary<int, SegmentAttributeDatum> GetAttributeValueHistory(string keyName, string keyValue, string attribute) => throw new NotImplementedException();
+        public List<SegmentAttributeDatum> GetAssetAttributes(string keyName, string keyValue)
+        {
+            // Check for the existence of the given key
+            if (!KeyProperties.ContainsKey(keyName))
+            {
+                throw new ArgumentException($"{keyName} not a key attribute in PennDOT network");
+            }
+
+            // Get the target segment info
+            var lookupSource = KeyProperties[keyName];
+            var targetSegment = lookupSource.FirstOrDefault(_ => _.KeyValue.Value == keyValue);
+            if (targetSegment == null) return new List<SegmentAttributeDatum>();
+            var segment = _unitOfWork.Context.MaintainableAsset
+                .Where(_ => _.Id == targetSegment.SegmentId)
+                .Include(_ => _.AggregatedResults)
+                .ThenInclude(_ => _.Attribute)
+                .FirstOrDefault();
+            if (segment == null) return new List<SegmentAttributeDatum>();
+            var attributeIdList = segment.AggregatedResults.Select(_ => _.AttributeId).Distinct();
+
+            // Populate the return value list
+            var returnValueList = new List<SegmentAttributeDatum>();
+
+            foreach (var attributeId in attributeIdList)
+            {
+                // Get the entry with the most recent value
+                var maxEntry = segment.AggregatedResults
+                    .Where(_ => _.AttributeId == attributeId)
+                    .OrderByDescending(_ => _.Year)
+                    .First();
+                string attributeValue = (maxEntry.Discriminator[0] == 'N') ? maxEntry.NumericValue.ToString() : maxEntry.TextValue;
+                returnValueList.Add(new SegmentAttributeDatum(maxEntry.Attribute.Name, attributeValue));
+            }
+            returnValueList.Add(new SegmentAttributeDatum("BRKEY", segment.FacilityName));
+            returnValueList.Add(new SegmentAttributeDatum("BMSID", segment.SectionName));
+
+            return returnValueList;
+        }
+        public Dictionary<int, SegmentAttributeDatum> GetAttributeValueHistory(string keyName, string keyValue, string attribute)
+        {
+            // Check for the existence of the given key
+            if (!KeyProperties.ContainsKey(keyName))
+            {
+                throw new ArgumentException($"{keyName} not a key attribute in PennDOT network");
+            }
+            var targetSegment = KeyProperties[keyName].FirstOrDefault(_ => _.KeyValue.Value == keyValue);
+            if (targetSegment == null) return new Dictionary<int, SegmentAttributeDatum>();
+
+            // Get the sought attribute id
+            var attributeInfo = _unitOfWork.Context.Attribute.FirstOrDefault(_ => _.Name == attribute);
+            if (attributeInfo == null)
+            {
+                throw new ArgumentException($"{attribute} was not found");
+            }
+
+            var result = new Dictionary<int, SegmentAttributeDatum>();
+            if (attribute == "BRKEY")
+            {
+                result.Add(0, new SegmentAttributeDatum("BRKEY", _unitOfWork.Context.MaintainableAsset.First(_ => _.Id == targetSegment.SegmentId).FacilityName));
+            }
+            else if (attribute == "BMSID")
+            {
+                result.Add(0, new SegmentAttributeDatum("BMSID", _unitOfWork.Context.MaintainableAsset.First(_ => _.Id == targetSegment.SegmentId).SectionName));
+            }
+            else
+            {
+                var attributeValues = _unitOfWork.Context.AggregatedResult
+                    .Where(_ => _.MaintainableAssetId == targetSegment.SegmentId && _.AttributeId == attributeInfo.Id);
+
+                foreach (var value in attributeValues)
+                {
+                    string yearValue = (value.Discriminator[0] == 'N') ? value.NumericValue.ToString() : value.TextValue;
+                    result.Add(value.Year, new SegmentAttributeDatum(attributeInfo.Name, yearValue));
+                }
+            }
+
+            return result;
+        }
     }
 }
---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/Repositories/PennDOTMaintainableAsseetDataRepositoryTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Moq;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL;
+using AppliedResearchAssociates.iAM.UnitTestsCore.TestData;
+using Microsoft.Extensions.Logging;
+
+namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests.Repositories
+{
+    public class PennDOTMaintainableAsseetDataRepositoryTests
+    {
+        private TestDataForPennDOTMaintainableAssetRepo _testData;
+        private UnitOfDataPersistenceWork _testRepo;
+        private Mock<IAMContext> _mockedContext;
+        private Mock<DbSet<MaintainableAssetEntity>> _mockedMaintainableAssetEntitySet;
+        private Mock<DbSet<AggregatedResultEntity>> _mockedAggregatedResultsEntitySet;
+
+        public PennDOTMaintainableAsseetDataRepositoryTests()
+        {
+            _testData = new TestDataForPennDOTMaintainableAssetRepo();
+            _mockedContext = new Mock<IAMContext>();
+
+            // From https://docs.microsoft.com/en-us/ef/ef6/fundamentals/testing/mocking?redirectedfrom=MSDN
+            _mockedMaintainableAssetEntitySet = new Mock<DbSet<MaintainableAssetEntity>>();
+            _mockedMaintainableAssetEntitySet.As<IQueryable<MaintainableAssetEntity>>().Setup(_ => _.Provider).Returns(_testData.MaintainableAssetsLibrary.Provider);
+            _mockedMaintainableAssetEntitySet.As<IQueryable<MaintainableAssetEntity>>().Setup(_ => _.Expression).Returns(_testData.MaintainableAssetsLibrary.Expression);
+            _mockedMaintainableAssetEntitySet.As<IQueryable<MaintainableAssetEntity>>().Setup(_ => _.ElementType).Returns(_testData.MaintainableAssetsLibrary.ElementType);
+            _mockedMaintainableAssetEntitySet.As<IQueryable<MaintainableAssetEntity>>().Setup(_ => _.GetEnumerator()).Returns(_testData.MaintainableAssetsLibrary.GetEnumerator());
+
+            _mockedAggregatedResultsEntitySet = new Mock<DbSet<AggregatedResultEntity>>();
+            _mockedAggregatedResultsEntitySet.As<IQueryable<AggregatedResultEntity>>().Setup(_ => _.Provider).Returns(_testData.AggregatedResultsLibrary.Provider);
+            _mockedAggregatedResultsEntitySet.As<IQueryable<AggregatedResultEntity>>().Setup(_ => _.Expression).Returns(_testData.AggregatedResultsLibrary.Expression);
+            _mockedAggregatedResultsEntitySet.As<IQueryable<AggregatedResultEntity>>().Setup(_ => _.ElementType).Returns(_testData.AggregatedResultsLibrary.ElementType);
+            _mockedAggregatedResultsEntitySet.As<IQueryable<AggregatedResultEntity>>().Setup(_ => _.GetEnumerator()).Returns(_testData.AggregatedResultsLibrary.GetEnumerator());
+
+            _mockedContext.Setup(_ => _.MaintainableAsset).Returns(_mockedMaintainableAssetEntitySet.Object);
+            _mockedContext.Setup(_ => _.AggregatedResult).Returns(_mockedAggregatedResultsEntitySet.Object);
+            var mockedRepo = new Mock<UnitOfDataPersistenceWork>((new Mock<IConfiguration>()).Object, _mockedContext.Object);
+            mockedRepo.Setup(_ => _.NetworkRepo.GetPennDotNetwork()).Returns(_testData.TestNetwork);
+            _testRepo = mockedRepo.Object;
+        }
+
+        [Fact]
+        public void GeneratesKeyPropertiesDictionaryWithNumericKey()
+        {
+            // Arrange
+            var checkGuid = new Guid("8f80c690-3088-4084-b0e5-a8e070000a06");
+
+            // Act
+            var repo = new PennDOTMaintainableAssetDataRepository(_testRepo);
+
+            // Assert
+            Assert.Equal(2, repo.KeyProperties.Count());
+            Assert.Equal(5, repo.KeyProperties["BRKEY"].Count());
+            Assert.NotNull(repo.KeyProperties["BRKEY"].FirstOrDefault(_ => _.KeyValue.Value == "2").SegmentId == checkGuid);
+            Assert.NotNull(repo.KeyProperties["BMSID"].FirstOrDefault(_ => _.KeyValue.Value == "13401256").SegmentId == checkGuid);
+        }
+
+        [Fact]
+        public void ReturnsSegmeentDataWithBRKey()
+        {
+            // Arrange
+            var repo = new PennDOTMaintainableAssetDataRepository(_testRepo);
+
+            // Act
+            var testSegment = repo.GetAssetAttributes("BRKEY", "2");
+
+            // Assert
+            Assert.Equal(1, testSegment.Where(_ => _.Name == "BRKEY").Count());
+            Assert.Equal(2, testSegment.First(_ => _.Name == "BRKEY").NumericValue);
+            Assert.Equal("2", testSegment.First(_ => _.Name == "BRKEY").Value);
+            Assert.Equal(15.4, testSegment.First(_ => _.Name == "Length").NumericValue);
+            Assert.Equal("First B", testSegment.First(_ => _.Name == "Name").TextValue);
+        }
+
+        [Fact]
+        public void ReturnsSegmeentDataWithBMSID()
+        {
+            // Arrange
+            var repo = new PennDOTMaintainableAssetDataRepository(_testRepo);
+
+            // Act
+            var testSegment = repo.GetAssetAttributes("BMSID", "13401256");
+
+            // Assert
+            Assert.Equal(1, testSegment.Where(_ => _.Name == "BRKEY").Count());
+            Assert.Equal(2, testSegment.First(_ => _.Name == "BRKEY").NumericValue);
+            Assert.Equal("2", testSegment.First(_ => _.Name == "BRKEY").Value);
+            Assert.Equal(15.4, testSegment.First(_ => _.Name == "Length").NumericValue);
+            Assert.Equal("First B", testSegment.First(_ => _.Name == "Name").TextValue);
+        }
+
+        [Fact]
+        public void HandlesUnmatchedKey()
+        {
+            // Arrange
+            var repo = new PennDOTMaintainableAssetDataRepository(_testRepo);
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => repo.GetAssetAttributes("Dummy", "0"));
+        }
+
+        [Fact]
+        public void HandlesNoSegmentFound()
+        {
+            // Should the system also remove the asset from KeyProperties if not found?  I think so.
+
+            // Arrange
+            var repo = new PennDOTMaintainableAssetDataRepository(_testRepo);
+
+            // Act
+            var testSegment = repo.GetAssetAttributes("BRKEY", "100");
+
+            // Assert
+            Assert.Equal(0, testSegment.Count());
+        }
+    }
+}

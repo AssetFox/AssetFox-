58073288d136f66a13a31078fee2931df9b54e12
William Rummler <w.a.rummler@gmail.com>
2021-04-14 14:39:31 -0500

    Handle the case when no cash flow distribution rule is applicable to the cost under consideration.

--- BridgeCareApp/AppliedResearchAssociates.iAM/Analysis/ReasonAgainstCashFlow.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM/Analysis/ReasonAgainstCashFlow.cs
@@ -4,6 +4,7 @@
     {
         Undefined,
         NotNeeded,
+        NoApplicableDistributionRule,
         ApplicableDistributionRuleIsForOnlyOneYear,
         LastYearOfCashFlowIsOutsideOfAnalysisPeriod,
         FutureEventScheduleIsBlocked,

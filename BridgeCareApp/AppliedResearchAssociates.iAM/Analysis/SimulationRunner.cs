2439770be292353c5f6f883432109be057174c84
William Jockusch <william.jockusch@sc-binc.com>
2021-06-14 16:20:46 -0500

    A much better fix

--- BridgeCareApp/AppliedResearchAssociates.iAM/Analysis/SimulationRunner.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM/Analysis/SimulationRunner.cs
@@ -529,8 +529,8 @@ namespace AppliedResearchAssociates.iAM.Analysis
 
                     context.Detail.TreatmentRejections.Add(new TreatmentRejectionDetail(treatment.Name, TreatmentRejectionReason.InvalidCost));
                     var messageBuilder = SimulationLogMessageBuilders.InvalidTreatmentCost(context.Section, treatment, cost, context.SimulationRunner.Simulation.Id);
-                    Send(messageBuilder, false);
-                    throw new SimulationException(messageBuilder.Message);
+                    Send(messageBuilder);
+                    return true;
                 });
 
                 if (feasibleTreatments.Count > 0)

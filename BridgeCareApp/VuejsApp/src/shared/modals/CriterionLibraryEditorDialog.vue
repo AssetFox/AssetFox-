dde55c5e2eb87c8d0434880dbd3dfedc289e67dc
Amruta Borgaonkar <aborgaonkar@ara.com>
2021-06-15 11:59:06 -0500

    Updated label.

--- BridgeCareApp/VuejsApp/src/shared/modals/CriterionLibraryEditorDialog.vue
+++ BridgeCareApp/VuejsApp/src/shared/modals/CriterionLibraryEditorDialog.vue
@@ -14,7 +14,7 @@
           <v-btn :disabled="stateSelectedCriterionLibrary.id === uuidNIL || !stateSelectedCriterionIsValid"
                  class="ara-blue-bg white--text"
                  @click="onBeforeSubmit(true)">
-            Save
+            Apply Changes
           </v-btn>
           <v-btn class="ara-orange-bg white--text"
                  @click="onSubmit(false)">

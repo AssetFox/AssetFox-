f18ca52a1c555172068e0537d011cf0d88c3174f
Amruta Borgaonkar <aborgaonkar@ara.com>
2021-09-01 10:37:49 -0500

    Json parsing API services.

--- BridgeCareApp/BridgeCareCore/Services/DefaultData/AnalysisDefaultDataService.cs
+++ BridgeCareApp/BridgeCareCore/Services/DefaultData/AnalysisDefaultDataService.cs
@@ -1,17 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 using BridgeCareCore.Interfaces.DefaultData;
 using BridgeCareCore.Models.DefaultData;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace BridgeCareCore.Services.DefaultData
 {
     public class AnalysisDefaultDataService : IAnalysisDefaultDataService
     {
-        public Task<AnalysisDefaultData> GetAnalysisDefaultData()
+        public async Task<AnalysisDefaultData> GetAnalysisDefaultData()
         {
-            throw new NotImplementedException();
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory ?? string.Empty,
+               "MetaData//DefaultData", "defaultDataAttributes.json");
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"{filePath} does not exist");
+            }
+
+            var content = File.ReadAllText(filePath);
+            var defaultDataJson = JObject.Parse(content).SelectToken("DefaultData");
+            var analysisDefaultDataJson = defaultDataJson?.SelectToken("Analysis").ToString();
+            var analysisDefaultData = JsonConvert.DeserializeObject<AnalysisDefaultData>(analysisDefaultDataJson);
+            return analysisDefaultData;
         }
     }
 }
--- BridgeCareApp/BridgeCareCore/Services/DefaultData/InvestmentDefaultDataService.cs
+++ BridgeCareApp/BridgeCareCore/Services/DefaultData/InvestmentDefaultDataService.cs
@@ -1,17 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 using BridgeCareCore.Interfaces.DefaultData;
 using BridgeCareCore.Models.DefaultData;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace BridgeCareCore.Services.DefaultData
 {
     public class InvestmentDefaultDataService : IInvestmentDefaultDataService
     {
-        public Task<InvestmentDefaultData> GetInvestmentDefaultData()
+        public async Task<InvestmentDefaultData> GetInvestmentDefaultData()
         {
-            throw new NotImplementedException();
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory ?? string.Empty,
+               "MetaData//DefaultData", "defaultDataAttributes.json");
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"{filePath} does not exist");
+            }
+
+            var content = File.ReadAllText(filePath);
+            var defaultDataJson = JObject.Parse(content).SelectToken("DefaultData");
+            var InvestmentDefaultDataJson = defaultDataJson?.SelectToken("Investment").ToString();
+            var InvestmentDefaultData = JsonConvert.DeserializeObject<InvestmentDefaultData>(InvestmentDefaultDataJson);
+            return InvestmentDefaultData;
         }
     }
 }

f949c2644bf446be6e70117de0a313866039253b
Amruta Borgaonkar <aborgaonkar@ara.com>
2021-09-23 15:08:17 -0500

    Added default for SpendingStrategy as "As Budget Permits". Updated case for properties for AnalysisDefaultData object.

--- BridgeCareApp/BridgeCareCore/Models/DefaultData/AnalysisDefaultData.cs
+++ BridgeCareApp/BridgeCareCore/Models/DefaultData/AnalysisDefaultData.cs
@@ -4,12 +4,14 @@ namespace BridgeCareCore.Models.DefaultData
 {
     public class AnalysisDefaultData
     {
-        public string weighting { get; set; }
+        public string Weighting { get; set; }
 
-        public OptimizationStrategy optimizationStrategy { get; set; }
+        public OptimizationStrategy OptimizationStrategy { get; set; }
 
-        public string benefitAttribute { get; set; }
+        public string BenefitAttribute { get; set; }
 
-        public int benefitLimit { get; set; }
+        public int BenefitLimit { get; set; }
+
+        public SpendingStrategy SpendingStrategy { get; set; }
     }
 }

76e5a76a0580d2f62eb2d28d7c3fac5af9a45e9a
sbhardwaj <sbhardwaj@ara.com>
2021-11-08 15:47:02 -0600

    changed url in oidc and startup.cs

--- BridgeCareApp/BridgeCareCore/Startup.cs
+++ BridgeCareApp/BridgeCareCore/Startup.cs
@@ -35,7 +35,7 @@ namespace BridgeCareCore
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .AllowCredentials()
-                .WithOrigins("http://localhost:8080", "https://v2.iam-deploy.com", "https://iam-demo.net/", "https://test.iam-deploy.com", "https://iam-bridgecare.dev.penndot.gov");
+                .WithOrigins("http://localhost:8080", "https://v2.iam-deploy.com", "https://iam-demo.net/", "https://test.iam-deploy.com", "https://bamsdevint.penndot.gov/");
             }));
 
             services.AddSecurityConfig(Configuration);
--- BridgeCareApp/VuejsApp/src/oidc-config.ts
+++ BridgeCareApp/VuejsApp/src/oidc-config.ts
@@ -1,7 +1,7 @@
 const oidcConfig: any = {
     authorizationEndpoint: 'https://oidcservicessyst.penndot.gov/affwebservices/CASSO/oidc/BAMS/authorize',
     clientId: 'a56bb9d5-190d-4873-afea-0767d82dd91c',
-    redirectUri: 'https://iam-bridgecare.dev.penndot.gov/Authentication/'
+    redirectUri: 'https://www.bamsdevint.penndot.gov/Authentication/'
 };
 
 export default oidcConfig;

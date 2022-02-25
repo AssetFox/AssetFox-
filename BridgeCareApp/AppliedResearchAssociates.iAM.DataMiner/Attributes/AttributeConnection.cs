414cd43e60d06a49c3fa3ff6433a1ab3d8910f8e
Chadwick Becker <cbecker@ara.com>
2022-02-25 10:26:54 -0600

    Added constraint to manage unique section location identifiers. Moved GetData columns to a specification in the AttributeConnection abstraction. Note these string column names are required for data ingestion. (metaData.json and NetworkDefinition.json).

--- BridgeCareApp/AppliedResearchAssociates.iAM.DataMiner/Attributes/AttributeConnection.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataMiner/Attributes/AttributeConnection.cs
@@ -9,6 +9,10 @@ namespace AppliedResearchAssociates.iAM.DataMiner.Attributes
 
         public abstract IEnumerable<IAttributeDatum> GetData<T>();
 
+        public const string DateColumnName = "DATE_";
+        public const string DataColumnName = "DATA_";
+        public const string LocationIdentifierString = "LOCATION_IDENTIFIER";
+
         protected AttributeConnection(Attribute attribute) => Attribute = attribute;
     }
 }

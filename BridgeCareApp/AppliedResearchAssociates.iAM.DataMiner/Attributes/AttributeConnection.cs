395f8b7ff71bbb922a91cc9810a8c7b1a2620b1a
Chadwick Becker <cbecker@ara.com>
2022-02-25 11:14:06 -0600

    Added comment to update folks on what needs to change in the metaData.json and networkDefinition.json files.

--- BridgeCareApp/AppliedResearchAssociates.iAM.DataMiner/Attributes/AttributeConnection.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataMiner/Attributes/AttributeConnection.cs
@@ -13,6 +13,9 @@ namespace AppliedResearchAssociates.iAM.DataMiner.Attributes
         public const string DataColumnName = "DATA_";
         public const string LocationIdentifierString = "LOCATION_IDENTIFIER";
 
+        // Commands in metaData.json should now look more like this:
+        // "SELECT CAST(BRKEY AS VARCHAR(MAX)) & '-' & BRIDGE_ID AS LOCATION_IDENTIFIER, CAST(INSPDATE AS DATETIME) AS DATE_, CAST(REPLACE(ADTTOTAL, ',', '') AS float) AS DATA_ FROM dbo.PennDot_Report_A WHERE (ISNUMERIC(ADTTOTAL) = 1)",
+
         protected AttributeConnection(Attribute attribute) => Attribute = attribute;
     }
 }

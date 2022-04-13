582fd9dcb798afdaaf68b4795263a578c1e91b01
William Jockusch <william.jockusch@sc-binc.com>
2022-04-13 15:59:43 -0500

    WIP . . . trying to put together a very simple worksheet

---
+++ BridgeCareApp/BridgeCareCore/Services/Treatment/TreatmentGeneralSectionGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using AppliedResearchAssociates.iAM.DTOs;
+using BridgeCareCore.Services.SummaryReport.Models;
+
+namespace BridgeCareCore.Services.Treatment
+{
+    public static class TreatmentGeneralSectionGenerator
+    {
+        public static ExcelRowModel TreatmentNameRow(TreatmentDTO dto)
+        {
+            var firstCell = ExcelValueModels.RichString("Treatment Name", true);
+            return ExcelRowModels.WithEntries(firstCell);
+        }
+        public static List<IExcelWorksheetContentModel> GeneralSectionRows(
+            TreatmentDTO dto
+            ) {
+            var r = new List<IExcelWorksheetContentModel>
+            {
+                TreatmentNameRow(dto),
+            };
+            return r;
+        }
+    }
+}
---
+++ BridgeCareApp/BridgeCareCore/Services/Treatment/TreatmentWorksheetGenerator.cs
@@ -0,0 +1,20 @@
+﻿using AppliedResearchAssociates.iAM.DTOs;
+using BridgeCareCore.Services.SummaryReport.Models;
+
+namespace BridgeCareCore.Services.Treatment
+{
+    public static class TreatmentWorksheetGenerator
+    {
+        public static ExcelWorksheetModel ExportModelForTreatment(TreatmentDTO treatment)
+        {
+            var tabName = treatment.Name;
+            var rows = TreatmentGeneralSectionGenerator.GeneralSectionRows(treatment);
+            var r = new ExcelWorksheetModel
+            {
+                TabName = tabName,
+                Content = rows,
+            };
+            return r;
+        }
+    }
+}

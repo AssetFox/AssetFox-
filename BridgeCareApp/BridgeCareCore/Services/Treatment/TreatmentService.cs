de2c5107c7da90cd226f1b197a702bbedcd5fbee
William Jockusch <william.jockusch@sc-binc.com>
2022-04-19 09:05:23 -0500

    First take on grabbing portions of treatment from spreadsheet. Unused and untested.

--- BridgeCareApp/BridgeCareCore/Services/Treatment/TreatmentService.cs
+++ BridgeCareApp/BridgeCareCore/Services/Treatment/TreatmentService.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities.Abstract;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities.LibraryEntities.Treatment;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Extensions;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
 using AppliedResearchAssociates.iAM.DTOs;
 using BridgeCareCore.Interfaces;
@@ -47,10 +51,59 @@ namespace BridgeCareCore.Services
             }
         }
 
+        private Dictionary<string, string> DetailsSectionAsDictionary(ExcelWorksheet worksheet)
+        {
+            Dictionary<string, string> r = new Dictionary<string, string>();
+            var costsRowIndex = 2;
+            while (costsRowIndex< 100 && worksheet.Cells[costsRowIndex, 1].Text.ToLower()!=TreatmentExportStringConstants.Costs.ToLower())
+            {
+                costsRowIndex++;
+            }
+            if (costsRowIndex == 100)
+            {
+                throw new Exception("Costs header cell not found!");
+            }
+            for (int i=2; i<costsRowIndex; i++)
+            {
+                var name = worksheet.Cells[costsRowIndex, 1].Text.ToLower();
+                var value = worksheet.Cells[costsRowIndex, 2].Text;
+                r[name] = value;
+            }
+            return r;
+        }
+
+        private static int ParseInt(string s, int defaultValue = 0)
+        {
+            if (int.TryParse(s, out int r))
+            {
+                return r;
+            }
+            return 0;
+        }
 
         public TreatmentImportResultDTO ImportLibraryTreatmentsFile(
             Guid treatmentLibraryId,
             ExcelPackage excelPackage) {
+            _unitOfWork.Context.DeleteAll<SelectableTreatmentEntity>(t => t.TreatmentLibraryId == treatmentLibraryId);
+
+            foreach (var worksheet in excelPackage.Workbook.Worksheets)
+            {
+                var worksheetName = worksheet.Name;
+                var dictionary = DetailsSectionAsDictionary(worksheet);
+                var description = dictionary.GetValueOrDefault(TreatmentExportStringConstants.TreatmentDescription.ToLowerInvariant());
+                var yearsBeforeSame = dictionary.GetValueOrDefault(TreatmentExportStringConstants.YearsBeforeSame.ToLowerInvariant());
+                var yearsBeforeAny = dictionary.GetValueOrDefault(TreatmentExportStringConstants.YearsBeforeAny.ToLowerInvariant());
+                var category = dictionary.GetValueOrDefault(TreatmentExportStringConstants.Category.ToLowerInvariant());
+                var assetType = dictionary.GetValueOrDefault(TreatmentExportStringConstants.AssetType.ToLowerInvariant());
+                var newTreatment = new TreatmentDTO
+                {
+                    Name = worksheetName,
+                    Id = Guid.NewGuid(),
+                    Description = description,
+                    ShadowForAnyTreatment = ParseInt(yearsBeforeAny),
+                    ShadowForSameTreatment = ParseInt(yearsBeforeSame),
+                };
+            }
             return null;
         }
     }

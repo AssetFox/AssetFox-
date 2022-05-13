ac2c9d724f05414eddcd4d132e21169a25d36b9d
Jacob Walter <jwalter@ara.com>
2022-05-13 08:45:06 -0400

    Added CommittedProjectDTO

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DTOs/Abstract/BaseCommittedProjectDTO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppliedResearchAssociates.iAM.DTOs.Abstract
+{
+    public abstract class BaseCommittedProjectDTO : BaseDTO
+    {
+        /// <summary>
+        /// The simulation that the committed project will be applied to
+        /// </summary>
+        public Guid SimulationId { get; set; }
+
+        /// <summary>
+        /// The budget in the simulation that this committed project uses to fund its work
+        /// </summary>
+        /// <remarks>
+        /// May be null when the project is not using funds that have been made available to
+        /// the simulation.  For example, when a capacity adding project replaces an asset.
+        /// </remarks>
+        public Guid? ScenarioBudgetId { get; set; }
+
+        /// <summary>
+        /// Stores specific location keys (i.e., asset identifiers, routes, etc.)
+        /// </summary>
+        public Dictionary<string, string> LocationKeys { get; set; } = new Dictionary<string, string>();
+
+        /// <summary>
+        /// The cost to apply the project
+        /// </summary>
+        /// <remarks>
+        /// May be 0 when costs are not using the funding available under the investment
+        /// parameters of the scenario
+        /// </remarks>
+        public double Cost { get; set; }
+
+        /// <summary>
+        /// The year in which the committed project will be applied (YYYY)
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// Verifies the LocationsKeys provided result in a valid location for the network type
+        /// </summary>
+        /// <returns>
+        /// An indication if the LocationKeys are valid
+        /// </returns>
+        public abstract bool VerifyLocation();
+    }
+}
---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DTOs/SectionCommittedProjectDTO.cs
@@ -0,0 +1,12 @@
+﻿using AppliedResearchAssociates.iAM.DTOs.Abstract;
+
+namespace AppliedResearchAssociates.iAM.DTOs
+{
+    public class SectionCommittedProjectDTO : BaseCommittedProjectDTO
+    {
+        // TODO:  Change this once the data source for section-based networks has been added
+        //        to ensure the key field is being added.  Also verify budget versus scenario
+        public override bool VerifyLocation() => LocationKeys.Count == 1 ? true : false;
+
+    }
+}

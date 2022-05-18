299fd287283af3eb89f7c2c1cd11ea7fbad5e1cf
William Jockusch <william.jockusch@sc-binc.com>
2022-05-18 16:58:26 -0500

    Groundwork for checking attribute validity

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Data/Attributes/NumericAttributeAggregationRules.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppliedResearchAssociates.iAM.Data.Attributes
+{
+    public static class NumericAttributeAggregationRules
+    {
+        public const string Predominant = "PREDOMINANT";
+
+        public static IEnumerable<string> ValidRules()
+        {
+            yield return Predominant;
+        }
+    }
+}

4c4d40529198b5fc0cd4025c7052a5ebc57cdc6f
William Jockusch <william.jockusch@sc-binc.com>
2022-05-26 11:43:58 -0500

    beginnings of DataSourceController, with constructor

---
+++ BridgeCareApp/BridgeCareCore/Controllers/DataSourceController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
+using BridgeCareCore.Controllers.BaseController;
+using BridgeCareCore.Interfaces;
+using BridgeCareCore.Security.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace BridgeCareCore.Controllers
+{
+    public class DataSourceController : BridgeCareCoreBaseController
+    {
+        private readonly IDataSourceRepository _dataSourceRepository;
+
+        public DataSourceController(
+            IEsecSecurity esecSecurity,
+            UnitOfDataPersistenceWork unitOfWork,
+            IHubService hubService,
+            IHttpContextAccessor contextAccessor,
+            IDataSourceRepository dataSourceRepository)
+            : base(esecSecurity,
+                  unitOfWork,
+                  hubService,
+                  contextAccessor) 
+        {
+            _dataSourceRepository = dataSourceRepository ?? throw new ArgumentNullException(nameof(dataSourceRepository));
+        }
+    }
+}

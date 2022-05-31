9ae1a5383e36757c45b86a3a76f057eb8204478e
William Jockusch <william.jockusch@sc-binc.com>
2022-05-31 14:25:41 -0500

    Grab good DoubleParseHelper from dying branch

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Data/Helpers/DoubleParseHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppliedResearchAssociates.iAM.Data.Helpers
+{
+    public static class DoubleParseHelper
+    {
+        public static double TryParseDouble(string input, double defaultValue)
+        {
+            if (double.TryParse(input, out double value)) {
+                return value;
+            }
+            return defaultValue;
+        }
+
+        public static double? TryParseNullableDouble(string input)
+        {
+            if (double.TryParse(input, out var value))
+            {
+                return value;
+            }
+            return null;
+        }
+    }
+}

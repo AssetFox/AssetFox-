c934bfe97ee25d27c996cf191c1fd264c3e46756
Shaun Shepherd <sshepherd@us.ara.com>
2022-06-02 15:09:27 -0600

    remove export button from perf curves

--- BridgeCareApp/VuejsApp/src/components/performance-curve-editor/performance-curve-editor-dialogs/ImportExportPerformanceCurvesDialog.vue
+++ BridgeCareApp/VuejsApp/src/components/performance-curve-editor/performance-curve-editor-dialogs/ImportExportPerformanceCurvesDialog.vue
@@ -18,7 +18,6 @@
                 <v-layout justify-center>
                     <v-btn @click='onSubmit(false)' class='ghd-white-bg ghd-blue Montserrat-font-family' flat>Cancel</v-btn>
                     <v-btn @click='onSubmit(true)' class='ghd-white-bg ghd-blue ghd-button Montserrat-font-family' outline>Upload</v-btn>
-                    <v-btn @click='onSubmit(true, true)' class='ghd-white-bg ghd-blue ghd-button Montserrat-font-family' outline>Export</v-btn>
                 </v-layout>
             </v-card-actions>
         </v-card>

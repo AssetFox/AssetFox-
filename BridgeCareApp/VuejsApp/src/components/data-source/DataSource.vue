aae948916cd2f2c20d2f5a836d62fac1f58c4a1d
Shaun Shepherd <sshepherd@us.ara.com>
2022-06-09 16:25:14 -0600

    adding ui elements to page

--- BridgeCareApp/VuejsApp/src/components/data-source/DataSource.vue
+++ BridgeCareApp/VuejsApp/src/components/data-source/DataSource.vue
@@ -1,6 +1,48 @@
 <template>
-    <v-layout column class="Montserrat-font-family">
-        Data Source
+    <v-layout column justify-start class="Montserrat-font-family pa-2">
+        <!-- <v-flex xs6> -->
+            <v-layout align-center class="vl-style" row>
+                <v-layout column>
+                    <v-subheader class="ghd-control-label ghd-md-gray">Data Source</v-subheader>
+                    <v-select
+                      class="ghd-select ghd-text-field ghd-text-field-border ds-style"
+                      outline
+                      outlined
+                    >
+                    </v-select>
+                </v-layout>
+                <v-btn class="ghd-white-bg ghd-blue" outline>Add Data Source</v-btn>
+            </v-layout>
+        <!-- </v-flex> -->
+        <v-layout column>
+            <v-subheader class="ghd-control-label ghd-md-gray">Source Type</v-subheader>
+            <v-select
+              class="ghd-select ghd-text-field ghd-text-field-border ds-style"
+              outline
+              outlined
+            >
+            </v-select>
+        </v-layout>
+        <v-layout column>
+            <v-subheader class="ghd-control-label ghd-md-gray">Connection String</v-subheader>
+            <v-layout justify-start>
+                    <v-flex xs6>
+                        <v-textarea
+                          label="Description"
+                          no-resize
+                          outline
+                        >
+                        </v-textarea>
+                    </v-flex>
+            </v-layout>
+        </v-layout>
+        <v-layout justify-center> 
+            <v-flex xs6>
+                <v-btn class="ghd-white-bg ghd-blue" flat>Cancel</v-btn>
+                <v-btn class="ghd-white-bg ghd-blue" outline>Test</v-btn>
+                <v-btn class="ghd-white-bg ghd-blue" outline>Save</v-btn>
+            </v-flex>
+        </v-layout>
     </v-layout>
 </template>
 
@@ -17,5 +59,10 @@ export default class DataSource extends Vue {
 </script>
 
 <style>
-
+.ds-style {
+    width: 50%;
+}
+.vl-style {
+    width: 800px;
+}
 </style>

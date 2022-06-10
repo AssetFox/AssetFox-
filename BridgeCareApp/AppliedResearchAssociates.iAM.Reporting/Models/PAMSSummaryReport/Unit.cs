241280bd10624ac33fcce7907b17745ab5dc2c35
Bimal Shah <Bimal.Shah@NuOrigin.com>
2022-06-10 10:37:44 -0500

    Deleted Unit class from Model

--- BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Models/PAMSSummaryReport/Unit.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AppliedResearchAssociates.iAM.Reporting.Models.PAMSSummaryReport
-{
-    /// <summary>Think of this class as void.</summary>
-    public class Unit
-    {
-        public static Unit Default => new Unit();
-    }
-}

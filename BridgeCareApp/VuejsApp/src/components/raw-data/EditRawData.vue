276224085b888b0790f9c68fa1b468b24132697a
Bryson Taylor <bryson.taylor@sc-binc.com>
2022-06-14 12:14:00 -0500

    changes to sidebar

--- BridgeCareApp/VuejsApp/src/components/raw-data/EditRawData.vue
+++ BridgeCareApp/VuejsApp/src/components/raw-data/EditRawData.vue
@@ -9,7 +9,7 @@
                 <div class="raw-data-list-header">
                     Raw Data
                 </div>
-                <v-list class="ghd-navigation-rawdata-list">
+                <v-list class="ghd-navigation-list">
                     <v-list-item-group
                         class="settings-list ghd-control-text"
                         :key="navigationTab.tabName"
@@ -26,14 +26,14 @@
                             <v-list-tile-content>
                                 <v-list-tile-title style="text-decoration: none">{{navigationTab.tabName}}</v-list-tile-title>
                             </v-list-tile-content>
-                            <v-list-tile-icon class="selected-sidebar-icon">
+                            <!-- <v-list-tile-icon class="selected-sidebar-icon">
                                     <v-icon class="mx-2 ghd-blue" slot="prependIcon">fa-thin fa-angle-right</v-icon>
-                            </v-list-tile-icon>
+                            </v-list-tile-icon> -->
                         </v-list-tile>
                     </v-list-item-group>
                 </v-list>
             </v-card>
-            <v-flex xs12 class="ghd-content">
+            <v-flex xs12 style="padding-left:200px !important">
                 <v-container fluid grid-list-xs style="padding-left:20px;padding-right:20px;">
                     <router-view></router-view>
                 </v-container>
@@ -177,11 +177,18 @@ export default class EditRawData extends Vue {
 }
 
 .primary--text .raw-data-icon{
-    stroke: #333333 !important;
+    stroke: #FFFFFF !important;
 }
 
 .raw-data-icon {
     stroke: #999999 !important;
 }
 
+.raw-data-svg-fill {
+    fill: #FFFFFF;
+}
+
+.primary--text .raw-data-svg-fill {
+    fill: #2A578D;
+}
 </style>

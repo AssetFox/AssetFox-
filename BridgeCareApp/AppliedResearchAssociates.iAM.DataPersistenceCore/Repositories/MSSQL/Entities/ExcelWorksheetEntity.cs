9f63fcc86cf34eb74ea3dc0f32c50639db19e288
William Jockusch <william.jockusch@sc-binc.com>
2022-06-15 09:19:10 -0500

    First take on a very minimalist ExcelWorksheetEntity

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/Entities/ExcelWorksheetEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities
+{
+    internal class ExcelWorksheetEntity
+    {
+        public Guid Id { get; set; }
+        public string SerializedWorksheetContent { get; set; }
+    }
+}

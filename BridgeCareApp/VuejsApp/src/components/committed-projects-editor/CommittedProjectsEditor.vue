d25f50b5f47fed054c70fa43319269a9adff196b
Shaun Shepherd <sshepherd@us.ara.com>
2022-06-16 15:35:09 -0600

    tweaked ui to match figma

--- BridgeCareApp/VuejsApp/src/components/committed-projects-editor/CommittedProjectsEditor.vue
+++ BridgeCareApp/VuejsApp/src/components/committed-projects-editor/CommittedProjectsEditor.vue
@@ -1,25 +1,32 @@
 <template>
     <v-layout column>
         <v-layout row>
-            <v-btn class="ghd-white-bg ghd-blue Montserrat-font-family" outline>Get Template</v-btn>
-            <v-btn class="ghd-white-bg ghd-blue Montserrat-font-family" outline>Import Projects</v-btn>
-            <v-btn class="ghd-white-bg ghd-blue Montserrat-font-family" outline>Exports Projects</v-btn>
-            <v-btn class="ghd-white-bg ghd-blue Montserrat-font-family" outline>Delete All</v-btn>
+            <v-btn class="ghd-white-bg ghd-blue ghd-button Montserrat-font-family" outline>Get Template</v-btn>
+            <v-btn class="ghd-white-bg ghd-blue ghd-button Montserrat-font-family" outline>Import Projects</v-btn>
+            <v-btn class="ghd-white-bg ghd-blue ghd-button Montserrat-font-family" outline>Exports Projects</v-btn>
+            <v-btn class="ghd-white-bg ghd-blue ghd-button Montserrat-font-family" outline>Delete All</v-btn>
         </v-layout>
         <v-checkbox class='Montserrat-font-family' label='No Treatments Before Committed Projects' />
         <v-subheader class="ghd-control-label ghd-md-gray Montserrat-font-family">Treatment Library</v-subheader>
-        <v-select
-          class="ghd-select ghd-text-field ghd-text-field-border Montserrat-font-family sel-style"
-        >
-        </v-select>
-        <v-text-field
-          append-icon="fas fa-search"
-          class="sel-style"
-          lablel="Search"
-          outlined
-        >
-        </v-text-field>
-        <v-layout justify-end>
+        <v-layout class="vl1-style">
+            <v-flex xs3 d-flex>
+         <v-select
+           outline
+           class="ghd-select ghd-text-field ghd-text-field-border pa-0 Montserrat-font-family"
+         >
+         </v-select>
+            </v-flex>
+            <v-flex xs3>
+         <v-text-field
+           prepend-inner-icon="fas fa-search"
+           label="Search"
+           solo
+           outline
+         >
+         </v-text-field>
+            </v-flex>
+        </v-layout>
+        <v-layout justify-end class="px-4">
         <p>Commited Projects: {{committedProjectsCount}}</p>
         </v-layout>
 
@@ -29,32 +36,48 @@
           :search="searchItems"
         >
         <template slot="items" slot-scope="props">
-                                        <td>
+                                        <td class="Montserrat-font-family btn-style">
                                             {{props.item.brkey}}
                                         </td>
-                                        <td>
+                                        <td class="Montserrat-font-family btn-style">
                                             {{props.item.year}}
                                         </td>
-                                        <td>
+                                        <td class="Montserrat-font-family btn-style">
                                             {{props.item.treatment}}
                                         </td>
-                                        <td>
+                                        <td class="Montserrat-font-family btn-style">
                                             {{props.item.budget}}
                                         </td>
-                                        <td>
+                                        <td class="Montserrat-font-family btn-style">
                                             {{props.item.cost}}
                                         </td>
+                                        <td class="Montserrat-font-family btn-style">
+                                            <v-icon class="ghd-blue">fas fa-trash</v-icon>
+                                        </td>
+        </template>
+        <template v-slot:footer>
+            <td>
+                <v-btn class="ghd-white-bg ghd-blue ghd-button Montserrat-font-family btn-style" outline>Add Committed Project</v-btn>    
+            </td>
         </template>
         </v-data-table>    
-        <v-btn class="ghd-white-bg ghd-blue ghd-button-text Montserrat-font-family btn-style" outline>Add Committed Project</v-btn>    
+
+
+        <v-layout justify-start align-center>
+            <v-text class="ghd-control-text Montserrat-font-family" v-if="totalDataFound > 0">Showing {{ dataPerPage }} of {{ totalDataFound }} results</v-text>
+            <v-text class="ghd-control-text Montserrat-font-family" v-else>No results found!</v-text>
+            <v-divider vertical class="mx-3"/>
+            <v-btn flat right
+                class="ghd-control-label ghd-blue"
+            > Delete Selected 
+            </v-btn>
+        </v-layout>
+<v-divider></v-divider>
         <v-layout justify-center row>
             <v-btn class="ghd-white-bg ghd-blue ghd-button-text Montserrat-font-family" flat>Cancel</v-btn>    
-            <v-btn class="ghd-white-bg ghd-blue ghd-button-text Montserrat-font-family" outline>Save</v-btn>    
+            <v-btn class="ghd-blue-bg ghd-white ghd-button Montserrat-font-family">Save</v-btn>    
         </v-layout>
     </v-layout>
-    <!-- <div>
-    CommittedProjects
-    </div> -->
 </template>
 <script lang="ts">
 import Vue from 'vue'
@@ -65,6 +88,8 @@ import { CommittedProject } from '@/shared/models/iAM/committed-projects';
 })
 export default class CommittedProjectsEditor extends Vue  {
     searchItems = '';
+    dataPerPage = 0;
+    totalDataFound = 0;
     cpItems: CommittedProject[] = [
         { 
             brkey: 1, 
@@ -94,7 +119,7 @@ export default class CommittedProjectsEditor extends Vue  {
             text: 'Year',
             value: 'year',
             align: 'left',
-            sortable: false,
+            sortable: true,
             class: 'header-border',
             width: '',
         },
@@ -123,20 +148,12 @@ export default class CommittedProjectsEditor extends Vue  {
             width: '',
         },
         {
-            text: 'Action',
+            text: 'Actions',
             value: 'actions',
             align: 'left',
             sortable: false,
             class: 'header-border',
-            width: '',
-        },
-        {
-            text: '',
-            value: '',
-            align: 'left',
-            sortable: false,
-            class: 'header-border',
-            width: '',
+            width: '10%',
         },
     ];
     committedProjectsCount: number = 0;
@@ -144,9 +161,18 @@ export default class CommittedProjectsEditor extends Vue  {
 </script>
 <style scoped>
 .sel-style {
-    width: 50%;
+    width: auto;
+    height: 56px;
+    padding: 20px;
 }
 .btn-style {
-    width: 500px;
+    width: 300px;
+    border-radius: 5px;
+}
+.header-border {
+  border-bottom: 2px solid black;
+}
+.vl1-style {
+justify-content: space-between;
 }
 </style>

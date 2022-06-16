dd7cc56a707a40466612e494b568beaee49f5ec1
Jacob Walter <jwalter@ara.com>
2022-06-16 15:20:03 -0400

    Initial adding committed project service tests

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/TestUtils/TestDataForCommittedProjects.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AppliedResearchAssociates.iAM.Analysis;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities;
+using AppliedResearchAssociates.iAM.DTOs;
+using AppliedResearchAssociates.iAM.DTOs.Enums;
+
+namespace AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils
+{
+    public class TestDataForCommittedProjects
+    {
+        public static List<string> KeyProperties => new List<string> { "ID", "BRKEY_", "BMSID" };
+
+        public static List<Simulation> Simulations => new List<Simulation>()
+        {
+            // Must be complete, but only need ID and associated libraries
+        };
+
+        public static List<AttributeEntity> Attributes => new List<AttributeEntity>()
+        {
+            // Must include name and ID
+        };
+
+        public static List<MaintainableAssetEntity> MaintainableAssets = new List<MaintainableAssetEntity>()
+        {
+            // Needs full MA with location
+        };
+
+        public static List<SectionCommittedProjectDTO> ValidCommittedProjects => new List<SectionCommittedProjectDTO>()
+        {
+
+        };
+
+        public static List<SectionCommittedProjectDTO> InvalidCommittedProjects => new List<SectionCommittedProjectDTO>()
+        {
+
+        };
+    }
+}
---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/CommittedProjects/CommittedProjectServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Moq;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Mappers;
+using BridgeCareCore.Services;
+using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories;
+
+namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests.CommittedProjects
+{
+    public class CommittedProjectServiceTests
+    {
+        private UnitOfDataPersistenceWork _testUOW;
+        private Mock<IAMContext> _mockedContext;
+
+        public CommittedProjectServiceTests()
+        {
+            _mockedContext = new Mock<IAMContext>();
+
+            var mockAssetDataRepository = new Mock<IAssetData>();
+            mockAssetDataRepository.Setup(_ => _.KeyProperties.Keys.ToList()).Returns(TestDataForCommittedProjects.KeyProperties);
+
+            _testUOW = new UnitOfDataPersistenceWork(new Mock<IConfiguration>().Object, _mockedContext.Object);
+        }
+    }
+}

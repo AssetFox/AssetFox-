772754e471bc3cfcc46dd2426aa2e5b91dc95dd8
William Jockusch <william.jockusch@sc-binc.com>
2022-06-16 10:39:54 -0500

    Introduce a DTO object for ExcelDatabaseWorksheets. Refine mapper.

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/ExcelWorksheetRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
+
+namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL
+{
+    public class ExcelWorksheetRepository
+    {
+        private readonly UnitOfDataPersistenceWork _unitOfWork;
+
+        public ExcelWorksheetRepository(UnitOfDataPersistenceWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void AddExcelWorksheet(ExcelWorksheetDTO dto)
+        {
+
+        }
+    }
+}
--- BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/Mappers/ExcelDatabaseWorksheetMapper.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/Mappers/ExcelDatabaseWorksheetMapper.cs
@@ -6,19 +6,37 @@ using System.Threading.Tasks;
 using AppliedResearchAssociates.iAM.Data.ExcelDatabaseStorage;
 using AppliedResearchAssociates.iAM.Data.ExcelDatabaseStorage.Serializers;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities;
+using AppliedResearchAssociates.iAM.DTOs;
 
 namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Mappers
 {
     public static class ExcelDatabaseWorksheetMapper
     {
-        public static ExcelWorksheetEntity ToEntity(this ExcelDatabaseWorksheet worksheet)
+        public static ExcelWorksheetEntity ToEntity(this ExcelSpreadsheetDTO dto)
+        {
+            var returnValue = new ExcelWorksheetEntity
+            {
+                Id = dto.Id,
+                SerializedWorksheetContent = dto.SerializedWorksheetContent,
+            };
+            return returnValue;
+        }
+
+        public static ExcelSpreadsheetDTO ToDTO (this ExcelDatabaseWorksheet worksheet)
         {
             var serializedContent = ExcelDatabaseWorksheetSerializer.Serialize(worksheet);
-            var returnValue = new ExcelWorksheetEntity
+            var returnValue = new ExcelSpreadsheetDTO
             {
                 Id = Guid.Empty,
                 SerializedWorksheetContent = serializedContent,
             };
+            return returnValue;
+        }
+
+        public static ExcelWorksheetEntity ToEntity(this ExcelDatabaseWorksheet worksheet)
+        {
+            var dto = worksheet.ToDTO();
+            var returnValue = dto.ToEntity();
             return returnValue;
         }
     }

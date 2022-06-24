788bad98077a2ba89946c29133ba5348286771de
Jacob Walter <jwalter@ara.com>
2022-06-24 16:57:33 -0400

    Reverted change to data source repo as it was not needed

--- BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/DataSourceRepository.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/DataSourceRepository.cs
@@ -41,8 +41,7 @@ namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL
         public List<string> GetDataSourceTypes()
         {
             var typesWithRepeats = _unitOfWork.Context.DataSource.Select(ds => ds.Type).ToList();
-            if (typesWithRepeats.Any()) return typesWithRepeats.Distinct().ToList();
-            return new List<string>();
+            return typesWithRepeats.Distinct().ToList();
         }
         public BaseDataSourceDTO GetDataSource(Guid id) =>
             _unitOfWork.Context.DataSource.FirstOrDefault(_ => _.Id == id)?.ToDTO();

dbb72d7e63c540df9b006e5469799250c563cbce
Bimal Shah <Bimal.Shah@NuOrigin.com>
2022-06-29 16:10:55 -0500

    Graph tabs and graph gneration code

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/PAMSSummaryReport/GraphTabs/AddGraphsInTabs.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using OfficeOpenXml;
+
+using AppliedResearchAssociates.iAM.Reporting.Interfaces.PAMSSummaryReport;
+using AppliedResearchAssociates.iAM.Reporting.Models.PAMSSummaryReport;
+
+using AppliedResearchAssociates.iAM.Reporting.Services.PAMSSummaryReport.GraphTabs.BPN;
+
+namespace AppliedResearchAssociates.iAM.Reporting.Services.PAMSSummaryReport.GraphTabs
+{
+    public class AddGraphsInTabs : IAddGraphsInTabs
+    {
+        private GraphData _graphData;
+
+        private IAddBPNGraphTab _addBPNGraphTab;
+        private IAddPoorCountGraphTab _addPoorCountGraphTab;
+
+        public AddGraphsInTabs()
+        {
+            _graphData = new GraphData();
+            _addBPNGraphTab = new AddBPNGraphTab();
+        }
+
+
+        private class GraphDataTab
+        {
+            public GraphDataTab(ExcelWorksheet workSheet, string title)
+            {
+                Worksheet = workSheet;
+                Title = title;
+            }
+            public string Title { get; }
+            public ExcelWorksheet Worksheet { get; }
+            public int DataColumn { get; set; } = 0;
+        }
+
+
+        public void Add(ExcelPackage excelPackage, ExcelWorksheet worksheet, ExcelWorksheet pamsWorkSummaryWorksheet,
+            ChartRowsModel chartRowModel, int simulationYearsCount)
+        {
+            var GraphDataDependentTabs = new Dictionary<string, GraphDataTab>();
+            void AddGraphDataDependentTab(string tabTitle, string graphTitle) => GraphDataDependentTabs.Add(tabTitle, new GraphDataTab(excelPackage.Workbook.Worksheets.Add(tabTitle), graphTitle));
+
+            // "Graph Data" tab is required for these, but we don't want "Graph Data" showing up until the end since it isn't really a report.
+            // Create the tabs and cache until "Graph Data" tab is available.
+            AddGraphDataDependentTab(PAMSConstants.IRI_BPN1_Tab, PAMSConstants.IRI_BPN1_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.IRI_BPN2_Tab, PAMSConstants.IRI_BPN2_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.IRI_BPN3_Tab, PAMSConstants.IRI_BPN3_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.IRI_BPN4_Tab, PAMSConstants.IRI_BPN4_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.IRI_Statewide_Tab, PAMSConstants.IRI_Statewide_Tab_Title);
+
+            AddGraphDataDependentTab(PAMSConstants.OPI_BPN1_Tab, PAMSConstants.OPI_BPN1_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.OPI_BPN2_Tab, PAMSConstants.OPI_BPN2_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.OPI_BPN3_Tab, PAMSConstants.OPI_BPN3_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.OPI_BPN4_Tab, PAMSConstants.OPI_BPN4_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.OPI_Statewide_Tab, PAMSConstants.OPI_Statewide_Tab_Title);
+
+            // Create the "Graph Data" tab
+            var graphDataWorksheet = excelPackage.Workbook.Worksheets.Add("Graph Data");
+
+            int startColumn = 1;
+            GraphDataDependentTabs[PAMSConstants.IRI_BPN1_Tab].DataColumn = startColumn;
+
+            startColumn = _graphData.Fill(graphDataWorksheet, pamsWorkSummaryWorksheet, chartRowModel.NHSPamsCountPercentSectionYearsRow, startColumn, simulationYearsCount);
+            GraphDataDependentTabs[PAMSConstants.IRI_BPN2_Tab].DataColumn = startColumn;
+
+            startColumn = _graphData.Fill(graphDataWorksheet, pamsWorkSummaryWorksheet, chartRowModel.NHSPamsCountPercentSectionYearsRow, startColumn, simulationYearsCount);
+            GraphDataDependentTabs[PAMSConstants.IRI_BPN3_Tab].DataColumn = startColumn;
+
+            startColumn = _graphData.Fill(graphDataWorksheet, pamsWorkSummaryWorksheet, chartRowModel.NHSPamsCountPercentSectionYearsRow, startColumn, simulationYearsCount);
+            GraphDataDependentTabs[PAMSConstants.IRI_BPN4_Tab].DataColumn = startColumn;
+
+            startColumn = _graphData.Fill(graphDataWorksheet, pamsWorkSummaryWorksheet, chartRowModel.NHSPamsCountPercentSectionYearsRow, startColumn, simulationYearsCount);
+            GraphDataDependentTabs[PAMSConstants.IRI_Statewide_Tab].DataColumn = startColumn;
+
+
+            startColumn = _graphData.Fill(graphDataWorksheet, pamsWorkSummaryWorksheet, chartRowModel.TotalPamsCountPercentYearsRow, startColumn, simulationYearsCount);
+            GraphDataDependentTabs[PAMSConstants.OPI_BPN1_Tab].DataColumn = startColumn;
+
+            startColumn = _graphData.Fill(graphDataWorksheet, pamsWorkSummaryWorksheet, chartRowModel.TotalPamsCountPercentYearsRow, startColumn, simulationYearsCount);
+            GraphDataDependentTabs[PAMSConstants.OPI_BPN2_Tab].DataColumn = startColumn;
+
+            startColumn = _graphData.Fill(graphDataWorksheet, pamsWorkSummaryWorksheet, chartRowModel.TotalPamsCountPercentYearsRow, startColumn, simulationYearsCount);
+            GraphDataDependentTabs[PAMSConstants.OPI_BPN3_Tab].DataColumn = startColumn;
+
+            startColumn = _graphData.Fill(graphDataWorksheet, pamsWorkSummaryWorksheet, chartRowModel.TotalPamsCountPercentYearsRow, startColumn, simulationYearsCount);
+            GraphDataDependentTabs[PAMSConstants.OPI_BPN4_Tab].DataColumn = startColumn;
+
+            startColumn = _graphData.Fill(graphDataWorksheet, pamsWorkSummaryWorksheet, chartRowModel.TotalPamsCountPercentYearsRow, startColumn, simulationYearsCount);
+            GraphDataDependentTabs[PAMSConstants.OPI_Statewide_Tab].DataColumn = startColumn;
+
+
+            _graphData.Fill(graphDataWorksheet, pamsWorkSummaryWorksheet, chartRowModel.NHSPamsCountPercentSectionYearsRow, startColumn, simulationYearsCount);
+            graphDataWorksheet.Hidden = eWorkSheetHidden.Hidden;
+
+            //// Fill the already created tabs that are dependent on the "Graph Data" tab; order of fill does not matter, worksheets were added in order
+            //foreach (var graphDataTab in GraphDataDependentTabs.Values)
+            //{
+            //    _conditionPercentageChart.Fill(graphDataTab.Worksheet, graphDataWorksheet, graphDataTab.DataColumn, graphDataTab.Title, simulationYearsCount);
+            //}
+                       
+            _addBPNGraphTab.AddBPNTab(excelPackage, worksheet, pamsWorkSummaryWorksheet, chartRowModel, simulationYearsCount);
+        }
+    }
+}
---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/PAMSSummaryReport/GraphTabs/BPN/AddBPNGraphTab.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+
+using AppliedResearchAssociates.iAM.Reporting.Interfaces.PAMSSummaryReport;
+using AppliedResearchAssociates.iAM.Reporting.Models.PAMSSummaryReport;
+
+namespace AppliedResearchAssociates.iAM.Reporting.Services.PAMSSummaryReport.GraphTabs.BPN
+{
+    public class AddBPNGraphTab : IAddBPNGraphTab
+    {
+        public AddBPNGraphTab()
+        {
+            
+        }
+
+        public void AddBPNTab(ExcelPackage excelPackage, ExcelWorksheet worksheet, ExcelWorksheet pamsWorkSummaryWorksheet, ChartRowsModel chartRowModel, int simulationYearsCount)
+        {
+            var GraphDataDependentTabs = new Dictionary<string, GraphDataTab>();
+
+            void AddGraphDataDependentTab(string tabTitle, string graphTitle) => GraphDataDependentTabs.Add(tabTitle, new GraphDataTab(excelPackage.Workbook.Worksheets.Add(tabTitle), graphTitle));
+
+            AddGraphDataDependentTab(PAMSConstants.IRI_BPN1_Tab, PAMSConstants.IRI_BPN1_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.IRI_BPN2_Tab, PAMSConstants.IRI_BPN2_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.IRI_BPN3_Tab, PAMSConstants.IRI_BPN3_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.IRI_BPN4_Tab, PAMSConstants.IRI_BPN4_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.IRI_Statewide_Tab, PAMSConstants.IRI_Statewide_Tab_Title);
+
+
+            AddGraphDataDependentTab(PAMSConstants.OPI_BPN1_Tab, PAMSConstants.OPI_BPN1_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.OPI_BPN2_Tab, PAMSConstants.OPI_BPN2_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.OPI_BPN3_Tab, PAMSConstants.OPI_BPN3_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.OPI_BPN4_Tab, PAMSConstants.OPI_BPN4_Tab_Title);
+            AddGraphDataDependentTab(PAMSConstants.OPI_Statewide_Tab, PAMSConstants.OPI_Statewide_Tab_Title);
+
+
+            GraphDataDependentTabs[PAMSConstants.IRI_BPN1_Tab].DataColumn = chartRowModel.TotalPamsPostedCountByBPNYearsRow;
+            GraphDataDependentTabs[PAMSConstants.IRI_BPN1_Tab].type = ChartType.CountChart;
+
+            GraphDataDependentTabs[PAMSConstants.IRI_BPN2_Tab].DataColumn = chartRowModel.TotalPamsPostedCountByBPNYearsRow;
+            GraphDataDependentTabs[PAMSConstants.IRI_BPN2_Tab].type = ChartType.CountChart;
+
+            GraphDataDependentTabs[PAMSConstants.IRI_BPN3_Tab].DataColumn = chartRowModel.TotalPamsPostedCountByBPNYearsRow;
+            GraphDataDependentTabs[PAMSConstants.IRI_BPN3_Tab].type = ChartType.CountChart;
+
+            GraphDataDependentTabs[PAMSConstants.IRI_BPN4_Tab].DataColumn = chartRowModel.TotalPamsPostedCountByBPNYearsRow;
+            GraphDataDependentTabs[PAMSConstants.IRI_BPN4_Tab].type = ChartType.CountChart;
+
+            GraphDataDependentTabs[PAMSConstants.IRI_Statewide_Tab].DataColumn = chartRowModel.TotalPamsPostedCountByBPNYearsRow;
+            GraphDataDependentTabs[PAMSConstants.IRI_Statewide_Tab].type = ChartType.CountChart;
+
+
+
+            GraphDataDependentTabs[PAMSConstants.OPI_BPN1_Tab].DataColumn = chartRowModel.TotalPamsPostedCountByBPNYearsRow;
+            GraphDataDependentTabs[PAMSConstants.OPI_BPN1_Tab].type = ChartType.CountChart;
+
+            GraphDataDependentTabs[PAMSConstants.OPI_BPN2_Tab].DataColumn = chartRowModel.TotalPamsPostedCountByBPNYearsRow;
+            GraphDataDependentTabs[PAMSConstants.OPI_BPN2_Tab].type = ChartType.CountChart;
+
+            GraphDataDependentTabs[PAMSConstants.OPI_BPN3_Tab].DataColumn = chartRowModel.TotalPamsPostedCountByBPNYearsRow;
+            GraphDataDependentTabs[PAMSConstants.OPI_BPN3_Tab].type = ChartType.CountChart;
+
+            GraphDataDependentTabs[PAMSConstants.OPI_BPN4_Tab].DataColumn = chartRowModel.TotalPamsPostedCountByBPNYearsRow;
+            GraphDataDependentTabs[PAMSConstants.OPI_BPN4_Tab].type = ChartType.CountChart;
+
+            GraphDataDependentTabs[PAMSConstants.OPI_Statewide_Tab].DataColumn = chartRowModel.TotalPamsPostedCountByBPNYearsRow;
+            GraphDataDependentTabs[PAMSConstants.OPI_Statewide_Tab].type = ChartType.CountChart;
+
+
+            foreach (var graphDataTab in GraphDataDependentTabs.Values)
+            {
+                switch (graphDataTab.type)
+                {
+                    case ChartType.AreaChart:
+                        //_bpnAreaChart.Fill(graphDataTab.Worksheet, pamsWorkSummaryWorksheet, graphDataTab.DataColumn, simulationYearsCount, graphDataTab.Title);
+                        break;
+
+                    case ChartType.CountChart:
+                        //_bpnCountChart.Fill(graphDataTab.Worksheet, pamsWorkSummaryWorksheet, graphDataTab.DataColumn, simulationYearsCount, graphDataTab.Title);
+                        break;
+
+                    case ChartType.CombinedChart:
+                        //_combinedPostedAndClosed.Fill(graphDataTab.Worksheet, pamsWorkSummaryWorksheet, graphDataTab.DataColumn, simulationYearsCount, graphDataTab.Title);
+                        break;
+
+                    case ChartType.CashChart:
+                        //_cashNeededByBPN.Fill(graphDataTab.Worksheet, pamsWorkSummaryWorksheet, graphDataTab.DataColumn, simulationYearsCount, graphDataTab.Title);
+                        break;
+                }
+            }
+        }
+
+        private class GraphDataTab
+        {
+            public GraphDataTab(ExcelWorksheet workSheet, string title)
+            {
+                Worksheet = workSheet;
+                Title = title;
+            }
+            public string Title { get; }
+            public ExcelWorksheet Worksheet { get; }
+            public int DataColumn { get; set; } = 0;
+            public ChartType type { get; set; }
+        }
+    }
+    public enum ChartType
+    {
+        CountChart,
+        AreaChart,
+        CashChart,
+        CombinedChart
+    }
+}

305f4d1fb57d4d743189b8978add8769560fc7f8
William Jockusch <william.jockusch@sc-binc.com>
2022-08-12 09:07:44 -0500

    Start on stress testing

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.StressTesting/FileReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace AppliedResearchAssociates.iAM.StressTesting
+{
+    public class FileReader
+    {
+        public static string ReadAllTextInGitIgnoredFile(string fileName)
+        {
+            var directory = Directory.GetCurrentDirectory();
+            var folder = Path.Combine(directory, "GitIgnored");
+            var file = Path.Combine(folder, fileName);
+            if (File.Exists(file))
+            {
+                var text = File.ReadAllText(file);
+                return text;
+            } else
+            {
+                var message = $"{file} does not exist!";
+                throw new Exception(message);
+            }
+        }
+    }
+}
---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.StressTesting/FileReaderTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AppliedResearchAssociates.iAM.StressTesting
+{
+    public class FileReaderTests
+    {
+        [Fact]
+        public void ReadSimulationOutput_Does()
+        {
+            var text = FileReader.ReadAllTextInGitIgnoredFile("SimulationOutput.json");
+        }
+    }
+}

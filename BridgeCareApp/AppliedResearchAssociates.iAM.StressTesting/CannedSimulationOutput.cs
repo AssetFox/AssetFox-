7026f2fff33339ca5aed5fd2dd48a0127fc954fa
William Jockusch <william.jockusch@sc-binc.com>
2022-08-16 10:42:27 -0500

    Remove portions of the stress tests

--- BridgeCareApp/AppliedResearchAssociates.iAM.StressTesting/CannedSimulationOutput.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.StressTesting/CannedSimulationOutput.cs
@@ -9,15 +9,5 @@ namespace AppliedResearchAssociates.iAM.StressTesting
     public static class CannedSimulationOutput
     {
         public const string Filename = "SimulationOutput.json";
-
-        public const string Filename1 = "SimulationOutput1Asset.json";
-
-        public const string Filename2 = "SimulationOutput2Assets.json";
-
-        public const string Filename10 = "SimulationOutput10Assets.json";
-
-        public const string Filename100 = "SimulationOutput100Assets.json";
-
-        public const string Filename600 = "SimulationOutput600Assets.json";
     }
 }

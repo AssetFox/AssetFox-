e0fa414630d7c72cd181e484b654e4cd4ac78608
William Jockusch <william.jockusch@sc-binc.com>
2022-09-19 11:49:12 -0500

    Migrated

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Migrations/20220919163430_useCharForDiscriminator.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Migrations
+{
+    public partial class useCharForDiscriminator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "AssetSummaryDetailValue",
+                type: "char(1)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "AssetDetailValue",
+                type: "char(1)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "AssetSummaryDetailValue",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "char(1)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "AssetDetailValue",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "char(1)");
+        }
+    }
+}

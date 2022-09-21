dffdba3ccac4175cdad6bd70737bee34366ad75d
William Jockusch <william.jockusch@sc-binc.com>
2022-09-21 15:37:06 -0500

    Basic test for getting a network. Passes.

--- BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/Network/NetworkCreationTests.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/Network/NetworkCreationTests.cs
@@ -15,6 +15,7 @@ using BridgeCareCore.Services;
 using AppliedResearchAssociates.iAM.Data.Attributes;
 using AppliedResearchAssociates.iAM.DataUnitTests.Tests;
 using AppliedResearchAssociates.iAM.DataUnitTests;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Mappers;
 
 namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests
 {
@@ -54,6 +55,34 @@ namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests
 
             var allNetworks = _testHelper.UnitOfWork.NetworkRepo.GetAllNetworks();
             var actual = allNetworks.Single(n => n.Id == networkId);
+        }
+
+        [Fact]
+        public void GetSimulationAnalysisNetwork_NetworkInDb_Does()
+        {
+            var config = _testHelper.Config;
+            var connectionString = TestConnectionStrings.BridgeCare(config);
+            var dataSourceDto = DataSourceTestSetup.DtoForSqlDataSourceInDb(_testHelper.UnitOfWork, connectionString);
+            var districtAttributeDomain = AttributeConnectionAttributes.String(connectionString, dataSourceDto.Id);
+            var districtAttribute = AttributeMapper.ToDto(districtAttributeDomain, dataSourceDto);
+            UnitTestsCoreAttributeTestSetup.EnsureAttributeExists(districtAttribute);
+
+            var networkName = RandomStrings.WithPrefix("Network");
+            var allDataSourceDto = AllDataSourceDtoFakeFrontEndFactory.ToAll(dataSourceDto);
+
+            var networkDefinitionAttribute = AllAttributeDtos.BrKey(allDataSourceDto);
+            var parameters = new NetworkCreationParameters
+            {
+                DefaultEquation = "[Deck_Area]",
+                NetworkDefinitionAttribute = networkDefinitionAttribute
+            };
+            var network = NetworkTestSetup.ModelForEntityInDbViaFactory(
+                _testHelper.UnitOfWork, districtAttributeDomain, parameters, networkName);
+            var explorer = _testHelper.UnitOfWork.AttributeRepo.GetExplorer();
+
+            var simulationAnalysisNetwork = _testHelper.UnitOfWork.NetworkRepo.GetSimulationAnalysisNetwork(network.Id, explorer);
+
+            Assert.Equal(network.Id, simulationAnalysisNetwork.Id);
         }
     }
 }

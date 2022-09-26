dade8140baddc16939e5d2861d757bf62e51df64
Jacob Walter <jwalter@ara.com>
2022-09-26 17:19:19 -0400

    Implemented Last functions for both text and numeric fields
    Related Work Items: #18944

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Data/Aggregation/LastTextAggregationRule.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AppliedResearchAssociates.iAM.Data.Attributes;
+
+namespace AppliedResearchAssociates.iAM.Data.Aggregation
+{
+    public class LastTextAggregationRule : TextAggregationRule
+    {
+        public override IEnumerable<(Attribute attribute, (int year, string value))> Apply(IEnumerable<IAttributeDatum> attributeData, Attributes.Attribute attribute)
+        {
+            var test = attributeData.Cast<AttributeDatum<string>>();
+            var distinctYears = attributeData.Select(_ => _.TimeStamp.Year).Distinct();
+            foreach (var distinctYear in distinctYears)
+            {
+                var currentYearAttributeDate = test.Where(_ => _.TimeStamp.Year == distinctYear);
+                yield return (attribute, (distinctYear, currentYearAttributeDate
+                    .OrderBy(__ => __.TimeStamp)
+                    .Select(__ => __.Value)
+                    .First()));
+            }
+        }
+    }
+}

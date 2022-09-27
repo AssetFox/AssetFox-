9833096038329cee3582155649ae9916e9cc786a
Shaun Shepherd <sshepherd@us.ara.com>
2022-09-27 11:33:00 -0600

    user criteria unit tests on authorize

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/APITestClasses/UserCriteriaTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
+using BridgeCareCore.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests.APITestClasses
+{
+    public class UserCriteriaTests
+    {
+        private static TestHelper _testHelper => TestHelper.Instance;
+
+        private UserCriteriaController CreateTestController(List<string> userClaims)
+        {
+            List<Claim> claims = new List<Claim>();
+            foreach (string claimstr in userClaims)
+            {
+                Claim claim = new Claim(ClaimTypes.Name, claimstr);
+                claims.Add(claim);
+            }
+            var testUser = new ClaimsPrincipal(new ClaimsIdentity(claims));
+            var controller = new UserCriteriaController(_testHelper.MockEsecSecurityAdmin.Object, _testHelper.UnitOfWork, _testHelper.MockHubService.Object, _testHelper.MockHttpContextAccessor.Object);
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = testUser }
+            };
+            return controller;
+        }
+
+        [Fact]
+        public async Task UserIsViewUserCriteriaAuthorized()
+        {
+
+        }
+    }
+}

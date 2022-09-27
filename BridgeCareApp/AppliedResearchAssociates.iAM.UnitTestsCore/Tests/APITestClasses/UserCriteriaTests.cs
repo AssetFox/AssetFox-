6d14520e35fe9da840befe11d8041f6923ea7836
Shaun Shepherd <sshepherd@us.ara.com>
2022-09-27 11:38:20 -0600

    add tests to user criteria

--- BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/APITestClasses/UserCriteriaTests.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/APITestClasses/UserCriteriaTests.cs
@@ -6,8 +6,11 @@ using System.Text;
 using System.Threading.Tasks;
 using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
 using BridgeCareCore.Controllers;
+using BridgeCareCore.Utils;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests.APITestClasses
@@ -36,7 +39,44 @@ namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests.APITestClasses
         [Fact]
         public async Task UserIsViewUserCriteriaAuthorized()
         {
-
+            // Admin authorized
+            // Arrange
+            var authorizationService = _testHelper.BuildAuthorizationService(services =>
+            {
+                services.AddAuthorization(options =>
+                {
+                    options.AddPolicy("ViewUserCriteriaClaim",
+                        policy => policy.RequireClaim(ClaimTypes.Name,
+                                                      BridgeCareCore.Security.SecurityConstants.Claim.UserCriteriaViewAccess));
+                });
+            });
+            var roleClaimsMapper = new RoleClaimsMapper();
+            var controller = CreateTestController(roleClaimsMapper.GetClaims(BridgeCareCore.Security.SecurityConstants.SecurityTypes.Esec, BridgeCareCore.Security.SecurityConstants.Role.Administrator));
+            // Act
+            var allowed = await authorizationService.AuthorizeAsync(controller.User, "ViewUserCriteriaClaim");
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+        [Fact]
+        public async Task UserIsModifyUserCriteriaAuthorized()
+        {
+            // Non-admin unauthorized
+            // Arrange
+            var authorizationService = _testHelper.BuildAuthorizationService(services =>
+            {
+                services.AddAuthorization(options =>
+                {
+                    options.AddPolicy("ModifyUserCriteriaClaim",
+                        policy => policy.RequireClaim(ClaimTypes.Name,
+                                                      BridgeCareCore.Security.SecurityConstants.Claim.UserCriteriaModifyAccess));
+                });
+            });
+            var roleClaimsMapper = new RoleClaimsMapper();
+            var controller = CreateTestController(roleClaimsMapper.GetClaims(BridgeCareCore.Security.SecurityConstants.SecurityTypes.Esec, BridgeCareCore.Security.SecurityConstants.Role.ReadOnly));
+            // Act
+            var allowed = await authorizationService.AuthorizeAsync(controller.User, "ModifyUserCriteriaClaim");
+            // Assert
+            Assert.False(allowed.Succeeded);
         }
     }
 }

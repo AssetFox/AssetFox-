e109b9fa7e7b288c8c44a455005dc00c4f6d8991
William Jockusch <william.jockusch@sc-binc.com>
2022-09-27 09:33:23 -0500

    Trying TestHelper as static only. Have not seen any hangs.

--- BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/TestUtils/TestHelper.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/TestUtils/TestHelper.cs
@@ -6,55 +6,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils
 {
-    public class TestHelper
+    public static class TestHelper
     {
-        private readonly IAMContext IamContext;
-
         static TestHelper()
-        {
-            lazy = new Lazy<TestHelper>(new TestHelper());
-        }
-
-        private UnitOfDataPersistenceWork UnitOfDataPersistenceWork { get; }
-
-        private TestHelper()
         {
             var config = TestConfiguration.Get();
             var connectionString = TestConnectionStrings.BridgeCare(config);
-            IamContext = new IAMContext(new DbContextOptionsBuilder<IAMContext>()
+            var dbContext = new IAMContext(new DbContextOptionsBuilder<IAMContext>()
                 .UseSqlServer(connectionString)
                 .Options);
 
-            UnitOfDataPersistenceWork = new UnitOfDataPersistenceWork(config, IamContext);
-
-            DatabaseResetter.ResetDatabase(UnitOfDataPersistenceWork);
+            UnitOfWork = new UnitOfDataPersistenceWork(config, dbContext);
+            DatabaseResetter.ResetDatabase(UnitOfWork);
         }
 
-        private static readonly Lazy<TestHelper> lazy;
-        private static TestHelper Instance
-        {
-            get
-            {
-                return lazy.Value;
-            }
-        }
+        public static IAMContext DbContext => UnitOfWork.Context;
 
-        public static IAMContext DbContext
-        {
-            get
-            {
-                var dbContext = Instance.IamContext;
-                return dbContext;
-            }
-        }
-
-        public static UnitOfDataPersistenceWork UnitOfWork
-        {
-            get
-            {
-                var unitOfWork = Instance.UnitOfDataPersistenceWork;
-                return unitOfWork;
-            }
-        }
+        public static readonly UnitOfDataPersistenceWork UnitOfWork;
     }
 }

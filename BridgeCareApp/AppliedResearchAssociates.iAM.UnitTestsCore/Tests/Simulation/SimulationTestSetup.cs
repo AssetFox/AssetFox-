45b901295313d4aafee2ae3b498894be0ff651a5
William Jockusch <william.jockusch@sc-binc.com>
2022-10-03 10:32:17 -0500

    Removed the last entity reference from SimulationTestSetup

--- BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/Simulation/SimulationTestSetup.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/Simulation/SimulationTestSetup.cs
@@ -15,21 +15,8 @@ using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
 
 namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests
 {
-    public static class SimulationTestSetup
+    public static class SimulationTestSetup // WjTestSetupDto
     {
-        public static SimulationEntity EntityInDb(UnitOfDataPersistenceWork unitOfWork, Guid networkId)
-        {
-            var name = RandomStrings.WithPrefix("Simulation");
-            var entity = new SimulationEntity
-            {
-                NetworkId = networkId,
-                Name = name,
-            };
-            unitOfWork.Context.Add(entity);
-            unitOfWork.Context.SaveChanges();
-            return entity;
-        }
-
         public static SimulationDTO TestSimulation(Guid? id = null, string name = null, Guid? owner = null)
         {
             var resolveName = name ?? RandomStrings.Length11();

60bc2a54bb48c997016cd8db66e561a541fb228b
Jacob Walter <jwalter@ara.com>
2022-10-05 09:23:06 -0400

    Created predominant rule for numeric aggregation

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Data/Aggregation/PredominantNumericAggregationRule.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AppliedResearchAssociates.iAM.Data.Attributes;
+
+namespace AppliedResearchAssociates.iAM.Data.Aggregation
+{
+    public class PredominantNumericAggregationRule : NumericAggregationRule
+    {
+        public override IEnumerable<(Attribute attribute, (int year, double value))> Apply(IEnumerable<IAttributeDatum> attributeData, Attribute attribute)
+        {
+            var test = attributeData.Cast<AttributeDatum<double>>();
+            var distinctYears = attributeData.Select(_ => _.TimeStamp.Year).Distinct();
+            foreach (var distinctYear in distinctYears)
+            {
+                var currentYearAttributeData = test.Where(_ => _.TimeStamp.Year == distinctYear);
+                yield return (attribute, (distinctYear, currentYearAttributeData
+                    .GroupBy(_ => _.Value)
+                    .OrderByDescending(group => group.Count())
+                    .Select(group => group.Key)
+                    .OrderByDescending(_ => _)
+                    .First()));
+            }
+        }
+    }
+}

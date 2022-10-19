03d52749d7fffe04fc38287f8dab15ba6ed8dc3e
William Jockusch <william.jockusch@sc-binc.com>
2022-10-19 09:47:22 -0500

    Pull the four authorization-related tests out into their own class

--- BridgeCareApp/BridgeCareCoreTests/Tests/Investment/InvestmentTests.cs
+++ BridgeCareApp/BridgeCareCoreTests/Tests/Investment/InvestmentTests.cs
@@ -127,18 +127,6 @@ namespace BridgeCareCoreTests.Tests
             return controller;
         }
 
-        private static ClaimsPrincipal CreateTestUser(List<string> userClaims)
-        {
-            List<Claim> claims = new List<Claim>();
-            foreach (string claimName in userClaims)
-            {
-                Claim claim = new Claim(ClaimTypes.Name, claimName);
-                claims.Add(claim);
-            }
-            var testUser = new ClaimsPrincipal(new ClaimsIdentity(claims));
-            return testUser;
-        }
-
         private void AddTestDataToDatabase()
         {
             var year = DateTime.Now.Year;
@@ -828,95 +816,7 @@ namespace BridgeCareCoreTests.Tests
                 !TestHelper.UnitOfWork.Context.BudgetAmount.Any(_ =>
                     _.Id == _testBudget.BudgetAmounts.ToList()[0].Id));
         }
-        [Fact]
-        public async Task UserIsViewInvestmentFromScenarioAuthorized()
-        {
-            // admin authorized
-            // Arrange
-            var authorizationService = BuildAuthorizationServiceMocks.BuildAuthorizationService(services =>
-            {
-                services.AddAuthorization(options =>
-                {
-                    options.AddPolicy(Policy.ViewInvestmentFromScenario,
-                        policy => policy.RequireClaim(ClaimTypes.Name,
-                                                      BridgeCareCore.Security.SecurityConstants.Claim.InvestmentViewAnyFromScenarioAccess));
-                });
-            });
-            var roleClaimsMapper = new RoleClaimsMapper();
-            var claims = roleClaimsMapper.GetClaims(BridgeCareCore.Security.SecurityConstants.SecurityTypes.Esec, BridgeCareCore.Security.SecurityConstants.Role.Administrator);
-            var user = CreateTestUser(claims);
-            // Act
-            var allowed = await authorizationService.AuthorizeAsync(user, Policy.ViewInvestmentFromScenario);
-            // Assert
-            Assert.True(allowed.Succeeded);
-        }
-        [Fact]
-        public async Task UserIsModifyInvestmentFromLibraryAuthorized()
-        {
-            // non-admin authorized
-            // Arrange
-            var authorizationService = BuildAuthorizationServiceMocks.BuildAuthorizationService(services =>
-            {
-                services.AddAuthorization(options =>
-                {
-                    options.AddPolicy(Policy.ModifyInvestmentFromLibrary,
-                        policy => policy.RequireClaim(ClaimTypes.Name,
-                                                      BridgeCareCore.Security.SecurityConstants.Claim.InvestmentModifyPermittedFromLibraryAccess));
-                });
-            });
-            var roleClaimsMapper = new RoleClaimsMapper();
-            var claims = roleClaimsMapper.GetClaims(BridgeCareCore.Security.SecurityConstants.SecurityTypes.Esec, BridgeCareCore.Security.SecurityConstants.Role.Editor);
-            var user = CreateTestUser(claims);
-            // Act
-            var allowed = await authorizationService.AuthorizeAsync(user, Policy.ModifyInvestmentFromLibrary);
-            // Assert
-            Assert.True(allowed.Succeeded);
 
-        }
-        [Fact]
-        public async Task UserIsImportInvestmentFromScenarioAuthorized()
-        {
-            // non-admin unauthorized
-            // Arrange
-            var authorizationService = BuildAuthorizationServiceMocks.BuildAuthorizationService(services =>
-            {
-                services.AddAuthorization(options =>
-                {
-                    options.AddPolicy(Policy.ImportInvestmentFromScenario,
-                        policy => policy.RequireClaim(ClaimTypes.Name,
-                                                      BridgeCareCore.Security.SecurityConstants.Claim.InvestmentImportPermittedFromScenarioAccess));
-                });
-            });
-            var roleClaimsMapper = new RoleClaimsMapper();
-            var claims = roleClaimsMapper.GetClaims(BridgeCareCore.Security.SecurityConstants.SecurityTypes.Esec, BridgeCareCore.Security.SecurityConstants.Role.ReadOnly);
-            var user = CreateTestUser(claims);
-            // Act
-            var allowed = await authorizationService.AuthorizeAsync(user, Policy.ImportInvestmentFromScenario);
-            // Assert
-            Assert.False(allowed.Succeeded);
-
-        }
-        [Fact]
-        public async Task UserIsViewInvestmentFromScenarioAuthorized_B2C()
-        {
-            // Arrange
-            var authorizationService = BuildAuthorizationServiceMocks.BuildAuthorizationService(services =>
-            {
-                services.AddAuthorization(options =>
-                {
-                    options.AddPolicy(Policy.ViewInvestmentFromScenario,
-                        policy => policy.RequireClaim(ClaimTypes.Name,
-                                                      BridgeCareCore.Security.SecurityConstants.Claim.InvestmentViewAnyFromScenarioAccess));
-                });
-            });
-            var roleClaimsMapper = new RoleClaimsMapper();
-            var claims = roleClaimsMapper.GetClaims(BridgeCareCore.Security.SecurityConstants.SecurityTypes.B2C, BridgeCareCore.Security.SecurityConstants.Role.Administrator);
-            var user = CreateTestUser(claims);
-            // Act
-            var allowed = await authorizationService.AuthorizeAsync(user, Policy.ViewInvestmentFromScenario);
-            // Assert
-            Assert.True(allowed.Succeeded);
-        }
         /**************************INVESTMENT BUDGETS EXCEL FILE IMPORT/EXPORT TESTS***********************************/
         [Fact]
         public async Task ShouldImportLibraryBudgetsFromFile()

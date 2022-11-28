a250fe939bd6f68000ba57f65337292940d5be3e
William Jockusch <william.jockusch@sc-binc.com>
2022-11-28 09:00:24 -0600

    Introduce "IntId" entities for the places we actually could gain

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/Entities/SimulationOutput/AssetSummaryDetailValueEntityIntId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities
+{
+    public class AssetSummaryDetailValueEntityIntId: SimulationOutputValueEntityIntId
+    {
+        public Guid AssetSummaryDetailId { get; set; }
+
+        public virtual AssetSummaryDetailEntity AssetSummaryDetail { get; set; }
+    }
+}

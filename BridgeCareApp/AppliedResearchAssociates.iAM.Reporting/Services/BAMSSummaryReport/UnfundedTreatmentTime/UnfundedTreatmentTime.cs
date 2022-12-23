8a342a4b2106e9bd32a9ce653419f4913e3f520c
John Malmberg <john.i.malmberg@gmail.com>
2022-12-23 15:58:51 -0600

    Prevent assets with no treatment from being removed from the unfunded treatment tabs.

--- BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/BAMSSummaryReport/UnfundedTreatmentTime/UnfundedTreatmentTime.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Reporting/Services/BAMSSummaryReport/UnfundedTreatmentTime/UnfundedTreatmentTime.cs
@@ -50,11 +50,13 @@ namespace AppliedResearchAssociates.iAM.Reporting.Services.BAMSSummaryReport.Unf
             var firstYear = true;
             foreach (var year in simulationOutput.Years.OrderBy(yr => yr.Year))
             {
-                var untreatedSections = _unfundedTreatmentCommon.GetUntreatedSections(year);
+                var untreatedSections = _unfundedTreatmentCommon.GetSectionsWithUnfundedTreatments(year);
+                var treatedSections = _unfundedTreatmentCommon.GetSectionsWithFundedTreatments(year);
 
                 if (firstYear)
                 {
-                    validFacilityIds.AddRange(untreatedSections.Select(_ => Convert.ToInt32(_summaryReportHelper.checkAndGetValue<double>(_.ValuePerNumericAttribute, "BRKEY_"))));
+                    validFacilityIds.AddRange(year.Assets.Select(_ => Convert.ToInt32(_summaryReportHelper.checkAndGetValue<double>(_.ValuePerNumericAttribute, "BRKEY_")))
+                        .Except(treatedSections.Select(_ => Convert.ToInt32(_summaryReportHelper.checkAndGetValue<double>(_.ValuePerNumericAttribute, "BRKEY_")))));
                     firstYear = false;
                     if (simulationOutput.Years.Count > 1)
                     {
@@ -63,7 +65,7 @@ namespace AppliedResearchAssociates.iAM.Reporting.Services.BAMSSummaryReport.Unf
                 }
                 else
                 {
-                    validFacilityIds = validFacilityIds.Intersect(untreatedSections.Select(_ => Convert.ToInt32(_summaryReportHelper.checkAndGetValue<double>(_.ValuePerNumericAttribute, "BRKEY_")))).ToList();
+                    validFacilityIds = validFacilityIds.Except(treatedSections.Select(_ => Convert.ToInt32(_summaryReportHelper.checkAndGetValue<double>(_.ValuePerNumericAttribute, "BRKEY_")))).ToList();
                 }
 
                 foreach (var section in untreatedSections)

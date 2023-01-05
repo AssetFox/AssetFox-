ab2673fddd13671aef72fb5513dfc06ee775317e
William Jockusch <william.jockusch@sc-binc.com>
2023-01-05 11:38:47 -0600

    Working on announcement tests. Still have a ways to go on them.

--- BridgeCareApp/BridgeCareCoreTests/Tests/AnnouncementTests.cs
+++ BridgeCareApp/BridgeCareCoreTests/Tests/AnnouncementTests.cs
@@ -1,31 +1,28 @@
 using System.Security.Claims;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Extensions;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Mappers;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
 using AppliedResearchAssociates.iAM.DTOs;
+using AppliedResearchAssociates.iAM.TestHelpers;
+using AppliedResearchAssociates.iAM.UnitTestsCore.Extensions;
 using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
 using BridgeCareCore.Controllers;
 using BridgeCareCore.Utils;
 using BridgeCareCoreTests.Helpers;
+using BridgeCareCoreTests.Tests.Announcement;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using Xunit;
 
 namespace BridgeCareCoreTests.Tests
 {
     public class AnnouncementTests
     {
-        private readonly AnnouncementController _controller;
-
-        public AnnouncementTests()
-        {
-            var accessor = HttpContextAccessorMocks.Default();
-            var hubService = HubServiceMocks.Default();
-            _controller = new AnnouncementController(EsecSecurityMocks.Admin, TestHelper.UnitOfWork,
-                hubService, accessor);
-        }
 
         public AnnouncementEntity TestAnnouncement(Guid? id = null)
         {
@@ -37,6 +34,18 @@ namespace BridgeCareCoreTests.Tests
                 Content = "Test Content"
             };
             return returnValue;
+        }
+
+        private AnnouncementDTO TestAnnouncementDto(Guid? id = null)
+        {
+            var resolveId = id ?? Guid.NewGuid();
+            var dto = new AnnouncementDTO
+            {
+                Content = "Test content",
+                Id = resolveId,
+                Title = "Test title",
+            };
+            return dto;
         }
         public AnnouncementController CreateTestController(List<string> userClaims)
         {
@@ -51,33 +60,62 @@ namespace BridgeCareCoreTests.Tests
             };
             return controller;
         }
+
+        private AnnouncementController CreateController(Mock<IUnitOfWork> unitOfWork)
+        {
+            var security = EsecSecurityMocks.AdminMock;
+            var hubService = HubServiceMocks.DefaultMock();
+            var contextAccessor = HttpContextAccessorMocks.DefaultMock();
+            var controller = new AnnouncementController(security.Object,
+                unitOfWork.Object,
+                hubService.Object,
+                contextAccessor.Object);
+            return controller;
+        }
         [Fact]
         public async Task ShouldReturnOkResultOnGet()
         {
             // Act
-            var result = await _controller.Announcements();
+            var unitOfWork = UnitOfWorkMocks.New();
+            var announcementRepository = AnnouncementRepositoryMocks.New(unitOfWork);
+            var userRepo = UserRepositoryMocks.EveryoneExists(unitOfWork);
+            var controller = CreateController(unitOfWork);
+            var result = await controller.Announcements();
 
             // Assert
             Assert.IsType<OkObjectResult>(result);
+            var _ = announcementRepository.SingleInvocationWithName(nameof(IAnnouncementRepository.Announcements));
         }
 
         [Fact]
         public async Task ShouldReturnOkResultOnPost()
         {
             // Act
-            var announcement = TestAnnouncement();
-            var dto = announcement.ToDto();
-            var result = await _controller.UpsertAnnouncement(dto);
+            var dto = TestAnnouncementDto();
+            var unitOfWork = UnitOfWorkMocks.New();
+            var announcementRepository = AnnouncementRepositoryMocks.New(unitOfWork);
+            var userRepo = UserRepositoryMocks.EveryoneExists(unitOfWork);
+            var controller = CreateController(unitOfWork);
+            var result = await controller.UpsertAnnouncement(dto);
 
             // Assert
             Assert.IsType<OkResult>(result);
+            var invocation = announcementRepository.SingleInvocationWithName(nameof(IAnnouncementRepository.UpsertAnnouncement));
+            var argument = invocation.Arguments[0];
+            ObjectAssertions.Equivalent(dto, argument);
         }
 
         [Fact]
         public async Task ShouldReturnOkResultOnDelete()
         {
             // Act
-            var result = await _controller.DeleteAnnouncement(Guid.Empty);
+
+            var unitOfWork = UnitOfWorkMocks.New();
+            var announcementRepository = AnnouncementRepositoryMocks.New(unitOfWork);
+            var userRepo = UserRepositoryMocks.EveryoneExists(unitOfWork);
+            var controller = CreateController(unitOfWork);
+            var announcementId = Guid.NewGuid();
+            var result = await controller.DeleteAnnouncement(announcementId);
 
             // Assert
             Assert.IsType<OkResult>(result);
@@ -88,80 +126,24 @@ namespace BridgeCareCoreTests.Tests
         {
             // Arrange
             var announcementId = Guid.NewGuid();
-            var announcement = TestAnnouncement(announcementId);
-            TestHelper.UnitOfWork.Context.AddEntity(announcement);
+            var announcement = TestAnnouncementDto(announcementId);
+            var announcements = new List<AnnouncementDTO> { announcement };
+            var unitOfWork = UnitOfWorkMocks.New();
+            var users = UserRepositoryMocks.EveryoneExists(unitOfWork);
+            var announcementRepository = AnnouncementRepositoryMocks.New(unitOfWork);
+            announcementRepository.Setup(a => a.Announcements()).Returns(announcements);
+            var controller = CreateController(unitOfWork);
 
             // Act
-            var result = await _controller.Announcements();
+            var result = await controller.Announcements();
 
             // Assert
             var okObjResult = result as OkObjectResult;
-            Assert.NotNull(okObjResult.Value);
-
             var dtos = (List<AnnouncementDTO>)Convert.ChangeType(okObjResult.Value, typeof(List<AnnouncementDTO>));
             Assert.True(dtos.Any());
             Assert.Equal(announcementId, dtos[0].Id);
         }
 
-        [Fact]
-        public async Task ShouldAddAnnouncementData()
-        {
-            // Arrange
-            var announcement = TestAnnouncement();
-            var dto = announcement.ToDto();
-
-            // Act
-            await _controller.UpsertAnnouncement(dto);
-
-            // Assert
-            var newDto = TestHelper.UnitOfWork.AnnouncementRepo.Announcements()[0];
-            Assert.Equal(dto.Id, newDto.Id);
-            Assert.Equal(dto.Title, newDto.Title);
-            Assert.Equal(dto.Content, newDto.Content);
-            Assert.Equal(dto.CreatedDate, newDto.CreatedDate);
-        }
-
-        [Fact]
-        public async Task ShouldModifyAnnouncementData()
-        {
-            // Arrange
-            var announcement = TestAnnouncement();
-            TestHelper.UnitOfWork.Context.AddEntity(announcement);
-            var getResult = await _controller.Announcements();
-            var dtos = (List<AnnouncementDTO>)Convert.ChangeType((getResult as OkObjectResult).Value, typeof(List<AnnouncementDTO>));
-
-            var dto = dtos[0];
-            dto.Title = "Updated Title";
-            dto.Content = "Updated Content";
-
-            // Act
-            await _controller.UpsertAnnouncement(dto);
-
-            // Assert
-            var modifiedDto = TestHelper.UnitOfWork.AnnouncementRepo.Announcements()[0];
-            Assert.Equal(dto.Id, modifiedDto.Id);
-            Assert.Equal(dto.Title, modifiedDto.Title);
-            Assert.Equal(dto.Content, modifiedDto.Content);
-            Assert.Equal(dto.CreatedDate, modifiedDto.CreatedDate);
-        }
-
-        [Fact]
-        public async Task ShouldDeletePerformanceCurveData()
-        {
-            // Arrange
-            var announcement = TestAnnouncement();
-            TestHelper.UnitOfWork.Context.AddEntity(announcement);
-            var getResult = await _controller.Announcements();
-            var dtos = (List<AnnouncementDTO>)Convert.ChangeType((getResult as OkObjectResult).Value, typeof(List<AnnouncementDTO>));
-
-            // Act
-            var result = _controller.DeleteAnnouncement(dtos[0].Id);
-
-            // Assert
-            Assert.IsType<OkResult>(result.Result);
-
-            Assert.True(!TestHelper.UnitOfWork.Context.Announcement.Any(_ => _.Id == dtos[0].Id));
-        }
         [Fact]
         public async Task UserIsViewAnnouncementAuthorized()
         {

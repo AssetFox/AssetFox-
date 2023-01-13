4db294e69d3248758c25fdda275ac226776ec063
William Jockusch <william.jockusch@sc-binc.com>
2023-01-13 13:50:13 -0600

    Created CriterionLibraryRepositoryTests from existing CriterionTests.

--- BridgeCareApp/BridgeCareCoreTests/Tests/CriterionTests.cs
+++ BridgeCareApp/BridgeCareCoreTests/Tests/CriterionTests.cs
@@ -4,19 +4,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Mappers;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
 using AppliedResearchAssociates.iAM.DTOs;
 using AppliedResearchAssociates.iAM.TestHelpers;
 using AppliedResearchAssociates.iAM.UnitTestsCore.Tests;
 using AppliedResearchAssociates.iAM.UnitTestsCore.Tests.Repositories;
 using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
 using BridgeCareCore.Controllers;
+using BridgeCareCore.Services;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Xunit;
 
 namespace BridgeCareCoreTests.Tests
 {
     public class CriterionTests
     {
+
+        private CriterionLibraryController CreateController(Mock<IUnitOfWork> unitOfWork)
+        {
+            var service = new CashFlowPagingService(unitOfWork.Object);
+            var security = EsecSecurityMocks.AdminMock;
+            var hubService = HubServiceMocks.DefaultMock();
+            var accessor = HttpContextAccessorMocks.DefaultMock();
+            var claimHelper = ClaimHelperMocks.New();
+            var controller = new CriterionLibraryController(
+                security.Object,
+                unitOfWork.Object,
+                hubService.Object,
+                accessor.Object
+                );
+            return controller;
+        }
         private CriterionLibraryController SetupController()
         {
             var unitOfWork = TestHelper.UnitOfWork;

9bdddb1d40800505184d37b1025c3f7aa3c21888
William Jockusch <william.jockusch@sc-binc.com>
2023-01-17 15:11:42 -0600

    Extracted authorization-related DeficientConditionGoal tests into their own class

---
+++ BridgeCareApp/BridgeCareCoreTests/Tests/DeficientConditionGoal/DeficientConditionGoalAuthorizationTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities.LibraryEntities.Deficient;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Entities.ScenarioEntities.Deficient;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL.Mappers;
+using AppliedResearchAssociates.iAM.DTOs;
+using AppliedResearchAssociates.iAM.TestHelpers;
+using AppliedResearchAssociates.iAM.UnitTestsCore.Tests;
+using AppliedResearchAssociates.iAM.UnitTestsCore.Tests.Repositories;
+using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
+using BridgeCareCore.Controllers;
+using BridgeCareCore.Models;
+using BridgeCareCore.Services;
+using BridgeCareCore.Utils;
+using BridgeCareCore.Utils.Interfaces;
+using MathNet.Numerics.Statistics.Mcmc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework.Internal.Execution;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using static BridgeCareCore.Security.SecurityConstants;
+using BridgeCareCoreTests.Helpers;
+
+namespace BridgeCareCoreTests.Tests
+{
+    public class DeficientConditionGoalAuthorizationTests
+    {
+        [Fact]
+        public async Task UserIsDeficientConditionGoalViewFromLibrary_Authorized()
+        {
+            // Arrange
+            var authorizationService = BuildAuthorizationServiceMocks.BuildAuthorizationService(services =>
+            {
+                services.AddAuthorization(options =>
+                {
+                    options.AddPolicy(Policy.ViewDeficientConditionGoalFromlLibrary,
+                        policy => policy.RequireClaim(ClaimTypes.Name,
+                                                      BridgeCareCore.Security.SecurityConstants.Claim.DeficientConditionGoalViewAnyFromLibraryAccess,
+                                                      BridgeCareCore.Security.SecurityConstants.Claim.DeficientConditionGoalViewPermittedFromLibraryAccess));
+                });
+            });
+            var roleClaimsMapper = new RoleClaimsMapper();
+            var claims = roleClaimsMapper.GetClaims(SecurityTypes.Esec, new List<string> { Role.Editor });
+            var user = ClaimsPrincipals.WithNameClaims(claims);
+            // Act
+            var allowed = await authorizationService.AuthorizeAsync(user, Policy.ViewDeficientConditionGoalFromlLibrary);
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+
+        [Fact]
+        public async Task UserIsDeficientConditionGoalModifyFromScenario_Authorized()
+        {
+            // Arrange
+            var authorizationService = BuildAuthorizationServiceMocks.BuildAuthorizationService(services =>
+            {
+                services.AddAuthorization(options =>
+                {
+                    options.AddPolicy(Policy.ModifyDeficientConditionGoalFromScenario,
+                        policy => policy.RequireClaim(ClaimTypes.Name,
+                                                      BridgeCareCore.Security.SecurityConstants.Claim.DeficientConditionGoalModifyAnyFromScenarioAccess,
+                                                      BridgeCareCore.Security.SecurityConstants.Claim.DeficientConditionGoalModifyPermittedFromScenarioAccess));
+                });
+            });
+            var roleClaimsMapper = new RoleClaimsMapper();
+            var claims = roleClaimsMapper.GetClaims(BridgeCareCore.Security.SecurityConstants.SecurityTypes.Esec, new List<string> { BridgeCareCore.Security.SecurityConstants.Role.Administrator });
+            var user = ClaimsPrincipals.WithNameClaims(claims);
+            // Act
+            var allowed = await authorizationService.AuthorizeAsync(user, Policy.ModifyDeficientConditionGoalFromScenario);
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+
+        [Fact]
+        public async Task UserIsDeficientConditionGoalModifyFromLibrary_Authorized()
+        {
+            // Arrange
+            var authorizationService = BuildAuthorizationServiceMocks.BuildAuthorizationService(services =>
+            {
+                services.AddAuthorization(options =>
+                {
+                    options.AddPolicy(Policy.ModifyDeficientConditionGoalFromLibrary,
+                        policy => policy.RequireClaim(ClaimTypes.Name,
+                                                      BridgeCareCore.Security.SecurityConstants.Claim.DeficientConditionGoalModifyPermittedFromLibraryAccess,
+                                                      BridgeCareCore.Security.SecurityConstants.Claim.DeficientConditionGoalModifyAnyFromLibraryAccess));
+                });
+            });
+            var roleClaimsMapper = new RoleClaimsMapper();
+            var claims = roleClaimsMapper.GetClaims(SecurityTypes.Esec, new List<string> { Role.ReadOnly });
+            var user = ClaimsPrincipals.WithNameClaims(claims);
+            // Act
+            var allowed = await authorizationService.AuthorizeAsync(user, Policy.ModifyDeficientConditionGoalFromLibrary);
+            // Assert
+            Assert.False(allowed.Succeeded);
+        }
+
+        [Fact]
+        public async Task UserIsDeficientConditionGoalViewFromLibrary_B2C_Authorized()
+        {
+            // Arrange
+            var authorizationService = BuildAuthorizationServiceMocks.BuildAuthorizationService(services =>
+            {
+                services.AddAuthorization(options =>
+                {
+                    options.AddPolicy(Policy.ViewDeficientConditionGoalFromlLibrary,
+                        policy => policy.RequireClaim(ClaimTypes.Name,
+                                                      BridgeCareCore.Security.SecurityConstants.Claim.DeficientConditionGoalViewAnyFromLibraryAccess,
+                                                      BridgeCareCore.Security.SecurityConstants.Claim.DeficientConditionGoalViewPermittedFromLibraryAccess));
+                });
+            });
+            var roleClaimsMapper = new RoleClaimsMapper();
+            var claims = roleClaimsMapper.GetClaims(BridgeCareCore.Security.SecurityConstants.SecurityTypes.B2C, new List<string> { BridgeCareCore.Security.SecurityConstants.Role.Administrator });
+            var user = ClaimsPrincipals.WithNameClaims(claims);
+            // Act
+            var allowed = await authorizationService.AuthorizeAsync(user, Policy.ViewDeficientConditionGoalFromlLibrary);
+            // Assert
+            Assert.True(allowed.Succeeded);
+        }
+    }
+}

556124893a0fac5e5bed8c01293fcd8643b3911d
William Jockusch <william.jockusch@sc-binc.com>
2023-01-20 06:58:26 -0600

    still wip

--- BridgeCareApp/BridgeCareCoreTests/Tests/PerformanceCurve/PerformanceCurveRepositoryMocks.cs
+++ BridgeCareApp/BridgeCareCoreTests/Tests/PerformanceCurve/PerformanceCurveRepositoryMocks.cs
@@ -4,15 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
 using Moq;
 
-namespace BridgeCareCoreTests.Tests.PerformanceCurve
+namespace BridgeCareCoreTests.Tests
 {
     public static class PerformanceCurveRepositoryMocks
     {
-        public static Mock<IPerformanceCurveRepository> New()
+        public static Mock<IPerformanceCurveRepository> New(Mock<IUnitOfWork> unitOfWork = null)
         {
             var mock = new Mock<IPerformanceCurveRepository>();
+            if (unitOfWork!=null)
+            {
+                unitOfWork.Setup(u => u.PerformanceCurveRepo).Returns(mock.Object);
+            }
             return mock;
         }
     }

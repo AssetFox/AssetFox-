d1b00ee6ca96dd4d59b16e4dfa8503ea868a9cb2
William Jockusch <william.jockusch@sc-binc.com>
2023-02-14 09:28:46 -0600

    Small test on CashFlowPagingService; too small really

---
+++ BridgeCareApp/BridgeCareCoreTests/Tests/CashFlowRule/CashFlowPagingServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
+using AppliedResearchAssociates.iAM.DTOs;
+using AppliedResearchAssociates.iAM.UnitTestsCore.Extensions;
+using BridgeCareCore.Models;
+using BridgeCareCore.Services;
+using BridgeCareCoreTests.Helpers;
+using Moq;
+using Xunit;
+
+namespace BridgeCareCoreTests.Tests.CashFlowRule
+{
+    public class CashFlowPagingServiceTests
+    {
+        private CashFlowPagingService CreatePagingService(Mock<IUnitOfWork> unitOfWork)
+        {
+            var service = new CashFlowPagingService(unitOfWork.Object);
+            return service;
+        }
+        [Fact]
+        public void GetSyncedScenarioDataset_Does()
+        {
+            var unitOfWork = UnitOfWorkMocks.New();
+            var repo = CashFlowRuleRepositoryMocks.DefaultMock(unitOfWork);
+            var pagingService = CreatePagingService(unitOfWork);
+            var scenarioId = Guid.NewGuid();
+            repo.Setup(r => r.GetScenarioCashFlowRules(scenarioId)).ReturnsEmptyList();
+            var syncModel = new PagingSyncModel<CashFlowRuleDTO>
+            {
+            };
+
+            var result = pagingService.GetSyncedScenarioDataSet(
+                scenarioId, syncModel);
+
+            Assert.Empty(result);
+        }
+    }
+}

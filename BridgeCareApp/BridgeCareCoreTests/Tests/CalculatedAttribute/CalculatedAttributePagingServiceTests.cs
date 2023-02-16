e75db7fe1d8f95d8115ec0abd8eb9ee3d554dabe
William Jockusch <william.jockusch@sc-binc.com>
2023-02-16 14:06:38 -0600

    Passing test for GetSyncedScenarioDataset with a single CalculatedAttribute

--- BridgeCareApp/BridgeCareCoreTests/Tests/CalculatedAttribute/CalculatedAttributePagingServiceTests.cs
+++ BridgeCareApp/BridgeCareCoreTests/Tests/CalculatedAttribute/CalculatedAttributePagingServiceTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
 using AppliedResearchAssociates.iAM.DTOs;
+using AppliedResearchAssociates.iAM.TestHelpers;
 using BridgeCareCore.Models;
 using BridgeCareCore.Services;
 using BridgeCareCoreTests.Helpers;
@@ -37,6 +38,31 @@ namespace BridgeCareCoreTests.Tests
             var result = pagingService.GetSyncedScenarioDataSet(simulationId, request);
 
             Assert.Empty(result);
+        }
+
+
+        [Fact]
+        public void GetSyncedScenarioDataset_RepoReturnsDto_ReturnsDto()
+        {
+            var unitOfWork = UnitOfWorkMocks.New();
+            var repo = CalculatedAttributeRepositoryMocks.New(unitOfWork);
+            var pagingService = CreatePagingService(unitOfWork);
+            var simulationId = Guid.NewGuid();
+            var id1 = Guid.NewGuid();
+            var equationCriterionPairId = Guid.NewGuid();
+            var equationId = Guid.NewGuid();
+            var dto = CalculatedAttributeDtos.Age(id1, equationCriterionPairId, equationId);
+            var cloneDto = CalculatedAttributeDtos.Age(id1, equationCriterionPairId, equationId);
+            repo.Setup(r => r.GetScenarioCalculatedAttributes(simulationId)).Returns(new List<CalculatedAttributeDTO> { dto });
+            var request = new CalculatedAttributePagingSyncModel
+            {
+
+            };
+
+            var result = pagingService.GetSyncedScenarioDataSet(simulationId, request);
+
+            var returnedDto = result.Single();
+            ObjectAssertions.Equivalent(cloneDto, returnedDto);
         }
     }
 }

8b740ee935afc56011349e74e68ee03a03ec58bc
William Jockusch <william.jockusch@sc-binc.com>
2023-02-16 13:49:09 -0600

    Bare-bones CalculatedAttributePagingService test. Passes.

---
+++ BridgeCareApp/BridgeCareCoreTests/Tests/CalculatedAttribute/CalculatedAttributePagingServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
+using AppliedResearchAssociates.iAM.DTOs;
+using BridgeCareCore.Models;
+using BridgeCareCore.Services;
+using BridgeCareCoreTests.Helpers;
+using Moq;
+using Xunit;
+
+namespace BridgeCareCoreTests.Tests
+{
+    public class CalculatedAttributePagingServiceTests
+    {
+        private CalculatedAttributePagingService CreatePagingService(Mock<IUnitOfWork> unitOfWork)
+        {
+            var service = new CalculatedAttributePagingService(unitOfWork.Object);
+            return service;
+        }
+
+        [Fact]
+        public void GetSyncedScenarioDataset_EverythingIsEmpty_Empty()
+        {
+            var unitOfWork = UnitOfWorkMocks.New();
+            var repo = CalculatedAttributeRepositoryMocks.New(unitOfWork);
+            var pagingService = CreatePagingService(unitOfWork);
+            var simulationId = Guid.NewGuid();
+            repo.Setup(r => r.GetScenarioCalculatedAttributes(simulationId)).Returns(new List<CalculatedAttributeDTO>());
+            var request = new CalculatedAttributePagingSyncModel
+            {
+
+            };
+
+            var result = pagingService.GetSyncedScenarioDataSet(simulationId, request);
+
+            Assert.Empty(result);
+        }
+    }
+}

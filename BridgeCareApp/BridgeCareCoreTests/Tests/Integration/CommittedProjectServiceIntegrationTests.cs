34fb510414f374d33c4fa855dbcadc8bda2df274
William Jockusch <william.jockusch@sc-binc.com>
2023-02-24 14:54:10 -0600

    wip

---
+++ BridgeCareApp/BridgeCareCoreTests/Tests/Integration/CommittedProjectServiceIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.UnitTestsCore.Tests;
+using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
+using BridgeCareCore.Services;
+using Xunit;
+
+namespace BridgeCareCoreTests.Tests.Integration
+{
+    public class CommittedProjectServiceIntegrationTests
+    {
+        private CommittedProjectService CreateCommittedProjectService()
+        {
+            var service = new CommittedProjectService(TestHelper.UnitOfWork);
+            return service;
+        }
+
+        [Fact]
+        public void GetTreatmentCost_Behaves()
+        {
+            NetworkTestSetup.CreateNetwork(TestHelper.UnitOfWork);
+            var service = CreateCommittedProjectService();
+            var treatmentLibraryId = Guid.NewGuid();
+            var assetKeyData = "";
+            var treatmentName = "treatment";
+
+            var cost = service.GetTreatmentCost(
+                treatmentLibraryId,
+                assetKeyData,
+                treatmentName,
+                NetworkTestSetup.NetworkId);
+        }
+    }
+}

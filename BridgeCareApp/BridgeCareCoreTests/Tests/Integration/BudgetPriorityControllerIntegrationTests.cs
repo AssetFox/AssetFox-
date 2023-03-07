e917f5284d082237e26bb97d7b80af06fc7d27b7
William Jockusch <william.jockusch@sc-binc.com>
2023-03-07 12:42:25 -0600

    This does it, but the test can be slow to run

--- BridgeCareApp/BridgeCareCoreTests/Tests/Integration/BudgetPriorityControllerIntegrationTests.cs
+++ BridgeCareApp/BridgeCareCoreTests/Tests/Integration/BudgetPriorityControllerIntegrationTests.cs
@@ -4,6 +4,7 @@ using AppliedResearchAssociates.iAM.UnitTestsCore.Tests;
 using AppliedResearchAssociates.iAM.UnitTestsCore.Tests.Repositories;
 using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
 using BridgeCareCore.Controllers;
+using BridgeCareCore.Models;
 using BridgeCareCore.Services;
 using BridgeCareCoreTests.Tests.BudgetPriority;
 using Xunit;
@@ -31,36 +32,38 @@ namespace BridgeCareCoreTests.Tests.Integration
         }
 
         [Fact]
-        public void UpsertBudgetPriorityLibrary_ChildUpdateFails_NoChanges()
+        public async Task UpsertBudgetPriorityLibrary_ChildUpdateFails_NoChanges()
         {
             AttributeTestSetup.CreateAttributes(TestHelper.UnitOfWork);
             NetworkTestSetup.CreateNetwork(TestHelper.UnitOfWork);
             var libraryId = Guid.NewGuid();
             var library = BudgetPriorityLibraryDtos.New(libraryId);
             var library2 = BudgetPriorityLibraryDtos.New();
-            var childDto = BudgetPriorityDtos.New();
+            library2.Name = "Updated name";
+            var priorityId = Guid.NewGuid();
+            var childDto = BudgetPriorityDtos.New(priorityId);
+            var childDto2 = BudgetPriorityDtos.New(priorityId);
             var budgetId = Guid.NewGuid();
             var budgetName = RandomStrings.WithPrefix("Budget");
-            var pairId = Guid.NewGuid();
-            var percentagePair = new BudgetPercentagePairDTO
-            {
-                BudgetId = budgetId,
-                BudgetName = budgetName,
-                Id = pairId,
-                Percentage = 2,
-            };
-            childDto.Year = int.MaxValue;
-            childDto.BudgetPercentagePairs.Add(percentagePair);
             var criterionLibrary = CriterionLibraryDtos.Dto();
             criterionLibrary.Name = null;
             childDto.CriterionLibrary = criterionLibrary;
             TestHelper.UnitOfWork.BudgetPriorityRepo.UpsertBudgetPriorityLibrary(library);
-            var budgetPriorities = new List<BudgetPriorityDTO> { childDto };
-            TestHelper.UnitOfWork.BudgetPriorityRepo.UpsertOrDeleteBudgetPriorities(
-                budgetPriorities, libraryId);
+            var budgetPriorities = new List<BudgetPriorityDTO> { childDto, childDto2 };
 
             var controller = CreateController();
-            
+            var upsertRequest = new LibraryUpsertPagingRequestModel<BudgetPriorityLibraryDTO, BudgetPriorityDTO>();
+            upsertRequest.Library = library;
+            var syncModel = new PagingSyncModel<BudgetPriorityDTO> { AddedRows = budgetPriorities };
+            upsertRequest.SyncModel = syncModel;
+
+            var exception = await Assert.ThrowsAnyAsync<Exception>(async () =>
+            await controller.UpsertBudgetPriorityLibrary(upsertRequest));
+
+            var librariesAfter = TestHelper.UnitOfWork.BudgetPriorityRepo.GetBudgetPriorityLibraries();
+            var libraryAfter = librariesAfter.Single(
+                lib => lib.Id == libraryId);
+            Assert.Equal(library.Name, libraryAfter.Name);
         }
     }
 }

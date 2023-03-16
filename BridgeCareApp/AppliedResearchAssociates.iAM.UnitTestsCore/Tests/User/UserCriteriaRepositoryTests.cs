6fc21e9573df56405d0f770e36e455f7cb75bdc3
William Jockusch <william.jockusch@sc-binc.com>
2023-03-16 15:38:30 -0500

    Two tests on UserCriteriaRepository

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/User/UserCriteriaRepositoryTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.DTOs;
+using AppliedResearchAssociates.iAM.TestHelpers;
+using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
+using Xunit;
+
+namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests.User
+{
+    public class UserCriteriaRepositoryTests
+    {
+        [Fact]
+        public async Task GetOwnUserCriteria_UserExists_Expected()
+        {
+            var user = await UserTestSetup.ModelForEntityInDb(TestHelper.UnitOfWork, true);
+            var userInfo = new UserInfoDTO
+            {
+                Sub = user.Username,
+                HasAdminAccess = true,
+            };
+
+            var userCriteria = TestHelper.UnitOfWork.UserCriteriaRepo.GetOwnUserCriteria(userInfo);
+            var expected = new UserCriteriaDTO
+            {
+                HasAccess = true,
+                HasCriteria = false,
+                UserId = user.Id,
+                UserName = user.Username,
+            };
+            ObjectAssertions.EquivalentExcluding(expected, userCriteria, x => x.CriteriaId);
+        }
+
+        [Fact]
+        public async Task GetOwnUserCriteria_UserDoesNotExist_Expected()
+        {
+            var username = RandomStrings.WithPrefix("User");
+            var userInfo = new UserInfoDTO
+            {
+                Sub = username,
+                HasAdminAccess = true,
+            };
+
+            var userCriteria = TestHelper.UnitOfWork.UserCriteriaRepo.GetOwnUserCriteria(userInfo);
+
+            var expected = new UserCriteriaDTO
+            {
+                HasAccess = true,
+                HasCriteria = false,
+                UserName = username,
+            };
+            ObjectAssertions.EquivalentExcluding(expected, userCriteria, x => x.CriteriaId, x => x.UserId);
+            var userInDb = await TestHelper.UnitOfWork.UserRepo.GetUserById(userCriteria.UserId);
+            Assert.NotNull(userInDb);
+        }
+    }
+}

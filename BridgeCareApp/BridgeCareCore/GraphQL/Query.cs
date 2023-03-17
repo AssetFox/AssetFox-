be6cd920e9819039c85771c2982c1cc3a675387e
Jacob Walter <jakedw7@gmail.com>
2023-03-17 12:31:44 -0400

    Working simulation list.  Note how DI works with hot chocolate

--- BridgeCareApp/BridgeCareCore/GraphQL/Query.cs
+++ BridgeCareApp/BridgeCareCore/GraphQL/Query.cs
@@ -1,23 +1,29 @@
-﻿using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories;
+﻿using System.Collections.Generic;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories;
 using AppliedResearchAssociates.iAM.DTOs;
+using HotChocolate;
 
 namespace BridgeCareCore.GraphQL
 {
     public class Query
     {
-        public MessageInfo GetMessage =>
-            new MessageInfo
-            {
-                Message = "This is a test",
-                Type = "Information"
-            };
+        //public MessageInfo GetMessage =>
+        //    new MessageInfo
+        //    {
+        //        Message = "This is a test",
+        //        Type = "Information"
+        //    };
+
+        public List<SimulationDTO> GetSimulations([Service(ServiceKind.Synchronized)] IUnitOfWork _unitOfWork) =>
+            _unitOfWork.SimulationRepo.GetAllScenario();
     }
 
     #region Testing
-    public class MessageInfo
-    {
-        public string Message { get; set; }
-        public string Type { get; set; }
-    }
+    //public class MessageInfo
+    //{
+    //    public string Message { get; set; }
+    //    public string Type { get; set; }
+    //}
     #endregion
 }

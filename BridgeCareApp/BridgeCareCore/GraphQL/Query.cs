66e56e39f1d9c99e882354e6aa053f10349d8c52
Jacob Walter <jakedw7@gmail.com>
2023-03-17 13:14:38 -0400

    Removed testing code

--- BridgeCareApp/BridgeCareCore/GraphQL/Query.cs
+++ BridgeCareApp/BridgeCareCore/GraphQL/Query.cs
@@ -8,24 +8,9 @@ namespace BridgeCareCore.GraphQL
 {
     public class Query
     {
-        //public MessageInfo GetMessage =>
-        //    new MessageInfo
-        //    {
-        //        Message = "This is a test",
-        //        Type = "Information"
-        //    };
-
         [UseFiltering]
         [UseSorting]
         public List<SimulationDTO> GetSimulations([Service(ServiceKind.Synchronized)] IUnitOfWork _unitOfWork) =>
             _unitOfWork.SimulationRepo.GetAllScenario();
     }
-
-    #region Testing
-    //public class MessageInfo
-    //{
-    //    public string Message { get; set; }
-    //    public string Type { get; set; }
-    //}
-    #endregion
 }

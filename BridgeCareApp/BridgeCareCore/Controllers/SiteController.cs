3a2e2f985034b5b79f07d8aa16d382e17a0dc5ff
chicks <chicks@ara.com>
2023-04-28 13:44:15 -0500

    Added SiteController

---
+++ BridgeCareApp/BridgeCareCore/Controllers/SiteController.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
+using AppliedResearchAssociates.iAM.DTOs;
+using BridgeCareCore.Controllers.BaseController;
+using AppliedResearchAssociates.iAM.Hubs;
+using AppliedResearchAssociates.iAM.Hubs.Interfaces;
+using BridgeCareCore.Security.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BridgeCareCore.Controllers
+{
+
+    [Route("api/site/[controller]")]
+    public class SiteController : BridgeCareCoreBaseController
+    {
+        public const string SiteError = "Site Error";
+
+        public SiteController(IEsecSecurity esecSecurity, IUnitOfWork unitOfWork, IHubService hubService, IHttpContextAccessor contextAccessor):
+                         base(esecSecurity, unitOfWork, hubService, contextAccessor) { }
+
+        [HttpGet]
+        [Route("GetImplementationName")]
+        [Authorize]
+        public async Task<IActionResult> GetImplementationName()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                HubService.SendRealTimeMessage(UserInfo.Name, HubConstant.BroadcastError, $"{SiteError}::GetImplementationName - {e.Message}");
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [Route("SetImplementationName/{name}")]
+        [Authorize]
+        public async Task<IActionResult> SetImplementationName(string name)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                HubService.SendRealTimeMessage(UserInfo.Name, HubConstant.BroadcastError, $"{SiteError}::SetImplementationName - {e.Message}");
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [Route("SetAgencyLogo/{agencyLogo}")]
+        [Authorize]
+        public async Task<IActionResult> SetAgencyLogo(string agencyLogo)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                HubService.SendRealTimeMessage(UserInfo.Name, HubConstant.BroadcastError, $"{SiteError}::SetAgencyLogo - {e.Message}");
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [Route("SetImplementationLogo/{productLogo}")]
+        [Authorize]
+        public async Task<IActionResult> SetImplementationLogo(string productLogo)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                HubService.SendRealTimeMessage(UserInfo.Name, HubConstant.BroadcastError, $"{SiteError}::SetImplementationLogo - {e.Message}");
+                throw;
+            }
+        }
+    }
+}

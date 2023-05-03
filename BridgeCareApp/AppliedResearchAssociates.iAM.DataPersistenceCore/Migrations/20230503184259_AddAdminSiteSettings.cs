6d928b38f6aca80e169e1f39e899c5b03eb3315f
Hugo Escobar <Hugo.EscobarDev@outlook.com>
2023-05-03 14:56:33 -0400

    Adds migration code

--- BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Migrations/20230503184259_AddAdminSiteSettings.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Migrations/20230503184259_AddAdminSiteSettings.cs
@@ -8,12 +8,24 @@ namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.CreateTable(
+               name: "AdminSiteSettings",
+               columns: table => new
+               {
+                   ID = table.Column<int>(type: "int", nullable: false)
+                       .Annotation("SqlServer:Identity", "1, 1"),
+                   ImplementationName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+               },
+               constraints: table =>
+               {
+                   table.PrimaryKey("PK_AdminSiteSettings", x => x.ID);
+               });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropTable(
+                name: "AdminSiteSettings");
         }
     }
 }

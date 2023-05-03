a287999e28e49f212847e241e8d89fa63cb1bbca
Hugo Escobar <Hugo.EscobarDev@outlook.com>
2023-05-03 14:45:51 -0400

    New migration files

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Migrations/20230503184259_AddAdminSiteSettings.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Migrations
+{
+    public partial class AddAdminSiteSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

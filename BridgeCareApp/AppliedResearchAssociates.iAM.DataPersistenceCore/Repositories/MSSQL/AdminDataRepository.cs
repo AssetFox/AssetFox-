91353f22c61cb4a9276fa1046e5c9a6c90f8d7e6
Martina Denis <mdenis@ara.com>
2023-05-10 14:50:03 -0400

    commit

--- BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/AdminDataRepository.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/MSSQL/AdminDataRepository.cs
@@ -33,6 +33,7 @@ namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories.MSSQL
 
         }
 
+
         public void SetPrimaryNetwork(string name)
         {
             var existingPrimaryNetwork = _unitOfWork.Context.AdminSettings.Where(_ => _.Key == "PrimaryNetwork").FirstOrDefault();

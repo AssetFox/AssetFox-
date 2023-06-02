b719f06b0e290ba27aeaf3e06e5fb4b62aadd418
Jacob Walter <jwalter@ara.com>
2023-06-02 16:29:28 -0400

    Document enums in DTO namespace

--- BridgeCareApp/AppliedResearchAssociates.iAM.DTOs/Enums/SpendingStrategy.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DTOs/Enums/SpendingStrategy.cs
@@ -1,37 +1,41 @@
 ﻿namespace AppliedResearchAssociates.iAM.DTOs.Enums
 {
     /// <summary>
-    /// .
+    /// Defines spending stratigies used by simulatiuon options
     /// </summary>
     public enum SpendingStrategy
     {
         /// <summary>
-        /// .
+        /// Spend no funding (apply no treatments)
         /// </summary>
         NoSpending,
 
         /// <summary>
-        /// .
+        /// Ignore budget levels and spend as required
         /// </summary>
         UnlimitedSpending,
 
         /// <summary>
-        /// .
+        /// Spend funding until both deficient and condition
+        /// goals are met.  Provided budget levels are ignored
         /// </summary>
         UntilTargetAndDeficientConditionGoalsMet,
 
         /// <summary>
-        /// .
+        /// Spend funding until condition goals are met.
+        /// Provided budget levels are ignored.
         /// </summary>
         UntilTargetConditionGoalsMet,
 
         /// <summary>
-        /// .
+        /// Spend funding until condition goals are met.
+        /// Provided budget levels are ignored.
         /// </summary>
         UntilDeficientConditionGoalsMet,
 
         /// <summary>
-        /// .
+        /// Spend funding until either no eligible treatments
+        /// remain or no budget remains.
         /// </summary>
         AsBudgetPermits,
     }
--- BridgeCareApp/AppliedResearchAssociates.iAM.DTOs/Enums/TreatmentEnum.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DTOs/Enums/TreatmentEnum.cs
@@ -8,37 +8,42 @@ namespace AppliedResearchAssociates.iAM.DTOs.Enums
     public class TreatmentDTOEnum
     {
         /// <summary>
-        /// .
+        /// Default treatment categories
         /// </summary>
+        // TODO: Replace with TreatmentEnum
         public enum TreatmentType
         {
             /// <summary>
-            /// .
+            /// Treatment is low-cost, low impact designed to preserve the condition of assets
+            /// already in good condition
             /// </summary>
             Preservation,
 
             /// <summary>
-            /// .
+            /// Primary reason for treatment is to add capacity, not to improve condition.
+            /// However, conditions will improve due to this treatment
             /// </summary>
             CapacityAdding,
 
             /// <summary>
-            /// .
+            /// Treatment is designed to address significant issues with the asset but not
+            /// fully replace the asset
             /// </summary>
             Rehabilitation,
 
             /// <summary>
-            /// .
+            /// The treatment will replace the exisiting asset
             /// </summary>
             Replacement,
 
             /// <summary>
-            /// .
+            /// Treatment is low-cost and usually applied according to a timed scehdule as opposed
+            /// to reacting to specific asset condiitons.
             /// </summary>
             Maintenance,
 
             /// <summary>
-            /// .
+            /// The treatment is applied for reasons outside of condition or capcity
             /// </summary>
             Other
         }
@@ -54,7 +59,7 @@ namespace AppliedResearchAssociates.iAM.DTOs.Enums
             Bridge,
 
             /// <summary>
-            /// A box culvert asset.
+            /// A culvert asset.
             /// </summary>
             Culvert
         }

26b6fca5b7de8118044a686a68e5c4ebb35ae727
Shaun Shepherd <sshepherd@ara.com>
2023-06-21 16:01:21 -0600

    adding raw data to admin pane

--- BridgeCareApp/VuejsApp/src/router.ts
+++ BridgeCareApp/VuejsApp/src/router.ts
@@ -121,6 +121,10 @@ const AdminData = () =>
 import(
     /*webpackChunkName: "AdminData" */ '@/components/admin-data/AdminData.vue'
 );
+const AdminRawData = () => 
+import(
+    '@/components/admin-rawdata/AdminRawData.vue'
+)
 
 const onHandlingUnsavedChanges = (to: any, next: any): void => {
     // @ts-ignore
@@ -363,6 +367,10 @@ const router = new VueRouter({
                     path: '/Site/',
                     component: Site,
                 },
+                {
+                    path: '/AdminRawData/',
+                    component: AdminRawData,
+                }
             ],
         },
         {

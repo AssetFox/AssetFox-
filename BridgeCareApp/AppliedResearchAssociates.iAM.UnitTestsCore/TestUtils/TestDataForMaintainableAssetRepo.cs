4b159ffbd9df1eaa7965c44dbed16e6bd3e4c8c2
Hugo Escobar <Hugo.EscobarDev@outlook.com>
2023-06-22 15:41:28 -0400

    Name change

--- BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/TestUtils/TestDataForMaintainableAssetRepo.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/TestUtils/TestDataForMaintainableAssetRepo.cs
@@ -29,12 +29,12 @@ namespace AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils
         {
             var entities = new List<AdminSettingsEntity>();
             // entities will need to be added here.
-            var FirstASection = new AdminSettingsEntity()
+            var KeyFields = new AdminSettingsEntity()
             {
                 Key = "KeyFields",
                 Value = "BRKEY_,BMSID"
             };
-            entities.Add(FirstASection);
+            entities.Add(KeyFields);
             return entities;
         }
 

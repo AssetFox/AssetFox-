b2473428c21e65e7207ac331d43ffaf37dd15d70
Jaden Nobles <jnobles@ara.com>
2023-06-27 12:50:06 -0400

    Revert admin data changes

--- BridgeCareApp/VuejsApp/src/components/admin-data/AdminData.vue
+++ BridgeCareApp/VuejsApp/src/components/admin-data/AdminData.vue
@@ -164,7 +164,6 @@ export default class Data extends Vue {
     simulationReportsDelimited: string = '';
     inventoryReportsDelimited: string = '';
 
-    reports: string[] = [];
     keyFieldsName: string  = 'KeyFields';
     simulationReportsName: string = 'SimulationReports';
     inventoryReportsName: string = 'InventoryReports';
@@ -195,17 +194,6 @@ export default class Data extends Vue {
     beforeDestroy() {
         this.setHasUnsavedChangesAction({ value: false });
     }
-
-    beforeMount()
-    {
-        this.getAvailableReportsAction();
-        
-    }
-    mounted() {
-        this.reports =  this.selectedAvailableReports;
-        this.simulationReports = clone(this.reports);
-        this.inventoryReports = clone(this.reports);
-    }
     //Watches
     @Watch('stateNetworks')
     onStateNetworksChanged(){
@@ -224,6 +212,10 @@ export default class Data extends Vue {
     @Watch('stateAvailableReportNames')
     onStateAvailableReportNamesChanged(){
         this.selectedAvailableReports = clone(this.stateAvailableReportNames);
+        const reports: string[] = this.stateAvailableReportNames;
+        this.simulationReports = clone(reports);
+        this.inventoryReports = clone(reports);
+
     }
 
     @Watch('stateInventoryReportNames')

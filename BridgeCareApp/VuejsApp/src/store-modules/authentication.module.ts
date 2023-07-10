e1de0e7b8f7bcd84b6196e8a28bdf94e8b44f9c5
Bobby Norton <rnorton@ara.com>
2023-07-10 12:21:34 -0400

    Commit All

--- BridgeCareApp/VuejsApp/src/store-modules/authentication.module.ts
+++ BridgeCareApp/VuejsApp/src/store-modules/authentication.module.ts
@@ -20,7 +20,7 @@ const state = {
     hasSimulationAccess: false,
     username: '',
     refreshing: false,
-    securityType: '',
+    securityType: 'B2C',
     pennDotSecurityType: 'ESEC',
     azureSecurityType: 'B2C',
 };

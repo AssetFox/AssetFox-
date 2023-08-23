62320095d382244b72fadcf50c29b1fcda3052c2
Shaun Shepherd <sshepherd@ara.com>
2023-08-23 15:47:47 -0600

    add criteria editor to page

--- BridgeCareApp/VuejsApp/src/components/reports/SummaryReport.vue
+++ BridgeCareApp/VuejsApp/src/components/reports/SummaryReport.vue
@@ -12,8 +12,61 @@
                         class="ghd-select ghd-text-field ghd-text-field-border">
                     </v-select>
                 </v-flex>
+                <v-flex>
+                    <v-btn>Generate Report</v-btn>
+                    <v-btn>Download Report</v-btn>
+                </v-flex>
             </v-layout>
         </v-flex>
+        <v-flex xs6>
+            <v-layout style="height=12px;padding-bottom:0px;">
+                <v-flex xs12 style="height=12px;padding-bottom:0px">
+                    <v-subheader class="ghd-control-label ghd-md-gray">                             
+                        Criteria
+                    </v-subheader>
+                </v-flex>
+                <v-flex xs1 style="height=12px;padding-bottom:0px;padding-top:0px;">
+                    <v-btn
+                        id="SummaryReport-criteriaEditor-btn"
+                        style="padding-right:20px !important;"
+                        @click="
+                            onShowCriterionEditorDialog
+                        "
+                        class="edit-icon ghd-control-label"
+                        icon
+                    >
+                        <img class='img-general' :src="require('@/assets/icons/edit.svg')"/>
+                    </v-btn>
+                </v-flex>
+            </v-layout>
+            <v-textarea
+                id="SummaryReport-criteria-textArea"
+                class="ghd-control-text ghd-control-border"
+                style="padding-bottom: 0px; height: 90px;"
+                no-resize
+                outline
+                readonly
+                :rows=6
+                v-model=mergedCriteriaExpression
+            >
+            </v-textarea>
+            <!-- <v-checkbox
+                id="SummaryReport-criteria-checkbox"
+                style="padding-top: 0px; margin-top: 4px;"
+                class="ghd-checkbox ghd-md-gray"
+                label="Criteria is intentionally empty (MUST check to Save)" 
+                v-model="criteriaIsIntentionallyEmpty"
+                v-show="criteriaIsEmpty()"
+            >
+            </v-checkbox> -->
+        </v-flex>
+        <v-flex>
+            <v-btn>Simulation Log</v-btn>
+        </v-flex>
+        <GeneralCriterionEditorDialog
+            :dialogData="criterionEditorDialogData"
+            @submit="onCriterionEditorDialogSubmit"
+        />
     </v-layout>
 </template>
 
@@ -24,11 +77,41 @@ import { clone, contains } from 'ramda';
 import { State } from 'vuex-class';
 import { isEqual } from '@/shared/utils/has-unsaved-changes-helper';
 import { getPropertyValues } from '@/shared/utils/getter-utils';
+import GeneralCriterionEditorDialog from '@/shared/modals/GeneralCriterionEditorDialog.vue';
+import { emptyGeneralCriterionEditorDialogData, GeneralCriterionEditorDialogData } from '@/shared/models/modals/general-criterion-editor-dialog-data';
 
-@Component
+@Component({
+    components: { GeneralCriterionEditorDialog },
+})
 export default class SummaryReports extends Vue {
 
     initializedBudgets: boolean = false;
+    mergedCriteriaExpression: string = "";
+
+    criterionEditorDialogData: GeneralCriterionEditorDialogData = clone(
+        emptyGeneralCriterionEditorDialogData,
+    );
+
+    onShowCriterionEditorDialog() {
+        this.criterionEditorDialogData = {
+            showDialog: true,
+            CriteriaExpression: this.mergedCriteriaExpression,
+        };
+    }
+    onCriterionEditorDialogSubmit(criterionexpression: string) {
+        this.criterionEditorDialogData = clone(
+            emptyGeneralCriterionEditorDialogData,
+        );
+
+        // if (!isNil(criterionexpression)) {
+        //     if(this.analysisMethod.criterionLibrary.id == getBlankGuid())
+        //         this.analysisMethod.criterionLibrary.id = getNewGuid();
+        //     this.analysisMethod = {
+        //         ...this.analysisMethod,
+        //         criterionLibrary: {...this.analysisMethod.criterionLibrary, mergedCriteriaExpression: criterionexpression} as CriterionLibrary,
+        //     };
+        // }
+    }
 }
 </script>
 

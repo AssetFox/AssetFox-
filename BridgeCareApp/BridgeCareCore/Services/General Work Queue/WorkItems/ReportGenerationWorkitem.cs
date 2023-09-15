e09239c60651060a066b1d93486adabd78268e68
Amruta Borgaonkar <aborgaonkar@ara.com>
2023-09-15 16:27:58 -0500

    Updated to get full error seen on UI

--- BridgeCareApp/BridgeCareCore/Services/General Work Queue/WorkItems/ReportGenerationWorkitem.cs
+++ BridgeCareApp/BridgeCareCore/Services/General Work Queue/WorkItems/ReportGenerationWorkitem.cs
@@ -66,7 +66,7 @@ namespace BridgeCareCore.Services
                 {
                     _log.Information($"Message: {message}");
                 }
-                throw new Exception($"Failed to generate '{reportName}' on simulation '{scenarioName}':: {report.Status}");
+                throw new Exception($"Failed to generate '{reportName}' on simulation '{scenarioName}': {report.Status}");
             }
 
             // Handle an incomplete run without errors

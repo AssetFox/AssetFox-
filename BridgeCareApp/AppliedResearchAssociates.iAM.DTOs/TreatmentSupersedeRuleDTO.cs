0d323442ec95bd62d354314336f0a04493987fec
Amruta Borgaonkar <aborgaonkar@ara.com>
2023-10-18 15:24:04 -0500

    TreatmentDTO updated and added TreatmentSupersedeRuleDTO

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DTOs/TreatmentSupersedeRuleDTO.cs
@@ -0,0 +1,11 @@
+﻿using AppliedResearchAssociates.iAM.DTOs.Abstract;
+
+namespace AppliedResearchAssociates.iAM.DTOs
+{
+    public class TreatmentSupersedeRuleDTO: BaseDTO
+    {
+        public TreatmentDTO treatment { get; set; } // Prevent treatment
+
+        public CriterionLibraryDTO CriterionLibrary { get; set; }
+    }
+}

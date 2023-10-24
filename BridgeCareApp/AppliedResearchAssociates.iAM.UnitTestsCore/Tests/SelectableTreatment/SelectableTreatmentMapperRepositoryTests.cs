4de5aa8976ee634c18b3a328cefac7f940b369fe
Amruta Borgaonkar <aborgaonkar@ara.com>
2023-10-24 17:31:15 -0500

    Unit tests - few more to be added.

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/SelectableTreatment/SelectableTreatmentMapperRepositoryTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests.SelectableTreatment
+{
+    public class SelectableTreatmentMapperRepositoryTests
+    {
+
+    }
+}

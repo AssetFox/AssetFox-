b0d54d9d4e0ee9469229db1e24dc747d44af7992
William Jockusch <william.jockusch@sc-binc.com>
2023-10-30 09:18:24 -0500

    This gets the delete test to pass via the stored procedure

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataUnitTests/DataTestSetup/RunBatch.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Data.SqlClient;
+using AppliedResearchAssociates.iAM.DataPersistenceCore.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using MathNet.Numerics.Optimization;
+using System.Reflection;
+
+namespace AppliedResearchAssociates.iAM.DataUnitTests
+{
+    public class RunBatch
+    {
+
+        private readonly UnitOfDataPersistenceWork _unitOfWork;
+  
+        public RunBatch(UnitOfDataPersistenceWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork ??
+                          throw new ArgumentNullException(nameof(unitOfWork));
+        }
+
+
+        public void ExecuteCommand(string connectionString)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(connectionString);
+                conn.Open();
+                var assembly = Assembly.GetExecutingAssembly();
+                var assemblyLocation = assembly.Location;
+                var assemblyFolder = Path.GetDirectoryName(assemblyLocation);
+                var folder = Path.Combine(assemblyFolder, "DataTestSetup", "SqlScripts");
+                DirectoryInfo dirInfo = new DirectoryInfo(folder);
+                var files = dirInfo.GetFiles("*.sql");
+
+                foreach (FileInfo f in files)
+                {
+                    string fname = f.Name;
+                    StreamReader fileReader = f.OpenText();
+                    string fileContent = fileReader.ReadToEnd();
+                    SqlCommand command = new SqlCommand(fileContent, conn);
+                    command.CommandTimeout = 600;
+                    int rowsAffected = command.ExecuteNonQuery();
+                    fileContent = string.Empty;
+                }
+
+                // Execute the stored procedure
+                _unitOfWork.Context.Database.SetCommandTimeout(TimeSpan.FromSeconds(4000));
+                _unitOfWork.Context.Database.ExecuteSqlRaw("EXEC usp_master_procedure;");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message + " Error occurred in RunBatch-ExecuteCommand.");
+            }
+
+        }
+    }
+
+}

b94a27a0dd8c4066ed8a7f9831909037e937547e
William Jockusch <william.jockusch@sc-binc.com>
2023-11-03 13:33:33 -0500

    Start on tests of AdminSettingsRepository

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/AdminSettings/AdminSettingsRepositoryTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
+using Xunit;
+
+namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests.AdminSettings
+{
+    public class AdminSettingsRepositoryTests
+    {
+        [Fact]
+        public void CreateAgencyLogo_Does()
+        {
+            var logoString = "agenlogo"; // length has to be a multiple of 4
+            var logoBytes = Convert.FromBase64String(logoString);
+
+            TestHelper.UnitOfWork.AdminSettingsRepo.SetAgencyLogo(logoBytes);
+
+            var fetchedLogo = TestHelper.UnitOfWork.AdminSettingsRepo.GetAgencyLogo();
+            Assert.EndsWith(logoString, fetchedLogo);
+        }
+
+        [Fact]
+        public void CreateImplementationLogo_Does()
+        {
+            var logoString = "implementatilogo";
+            var logoBytes = Convert.FromBase64String(logoString);
+
+            TestHelper.UnitOfWork.AdminSettingsRepo.SetImplementationLogo(logoBytes);
+
+            var fetchedLogo = TestHelper.UnitOfWork.AdminSettingsRepo.GetImplementationLogo ();
+            Assert.EndsWith(logoString, fetchedLogo);
+        }
+
+        [Fact]
+        public void SetPrimaryNetwork_Does()
+        {
+            var networkEntity = NetworkTestSetup.CreateNetwork(TestHelper.UnitOfWork);
+
+            TestHelper.UnitOfWork.AdminSettingsRepo.SetPrimaryNetwork(networkEntity.Name);
+
+            var primaryNetwork = TestHelper.UnitOfWork.AdminSettingsRepo.GetPrimaryNetwork();
+            Assert.Equal(networkEntity.Name, primaryNetwork);
+        }
+
+        [Fact]
+        public void SetRawDataNetwork_Does()
+        {
+            var networkEntity = NetworkTestSetup.CreateNetwork(TestHelper.UnitOfWork);
+
+            TestHelper.UnitOfWork.AdminSettingsRepo.SetRawDataNetwork(networkEntity.Name);
+
+            var rawDataNetwork = TestHelper.UnitOfWork.AdminSettingsRepo.GetRawDataNetwork();
+            Assert.Equal(networkEntity.Name, rawDataNetwork);
+        }
+    }
+}

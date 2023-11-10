de689b1589e41a5976a65b21bfb8448b69c5b313
HPurcell <hpurcell@ara.com>
2023-11-10 14:29:18 -0500

    Added migration for BudgetPercentagePair BudgetID col Removal

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Migrations/20231110171035_RemoveColumnBudgetIdFromTableBudgetPercentagePair.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Migrations
+{
+    public partial class RemoveColumnBudgetIdFromTableBudgetPercentagePair : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE [BudgetPercentagePair] NOCHECK CONSTRAINT all;");
+            migrationBuilder.Sql("ALTER TABLE [BudgetPercentagePair] DROP CONSTRAINT IF EXISTS [FK_BudgetPercentagePair_Budget_BudgetId];");
+            migrationBuilder.Sql("DROP INDEX IF EXISTS [IX_BudgetPercentagePair_BudgetId] ON [BudgetPercentagePair]");
+            migrationBuilder.Sql("ALTER TABLE [BudgetPercentagePair] DROP COLUMN IF EXISTS [BudgetId];");
+            migrationBuilder.Sql("ALTER TABLE [BudgetPercentagePair] WITH CHECK CHECK CONSTRAINT all;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

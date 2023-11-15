c2024b7711a9108f1a5517a8cd258a3b8cc7b680
HPurcell <hpurcell@ara.com>
2023-11-15 13:52:07 -0500

    Remove Orphans from Existing DB

--- BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Migrations/20231110171035_RemoveColumnBudgetIdFromTableBudgetPercentagePair.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Migrations/20231110171035_RemoveColumnBudgetIdFromTableBudgetPercentagePair.cs
@@ -8,6 +8,9 @@ namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+
+            migrationBuilder.Sql("Delete FROM dbo.[BudgetPercentagePair] WHERE NOT EXISTS(SELECT * FROM dbo.[ScenarioBudget] AS parent WHERE parent.Id = [BudgetPercentagePair].ScenarioBudgetId);");
+            migrationBuilder.Sql("Delete FROM dbo.[BudgetPercentagePair] WHERE NOT EXISTS(SELECT * FROM dbo.[ScenarioBudgetPriority] AS parent WHERE parent.Id = [BudgetPercentagePair].ScenarioBudgetPriorityId);");
             migrationBuilder.Sql("ALTER TABLE [BudgetPercentagePair] NOCHECK CONSTRAINT all;");
             migrationBuilder.Sql("ALTER TABLE [BudgetPercentagePair] DROP CONSTRAINT IF EXISTS [FK_BudgetPercentagePair_Budget_BudgetId];");
             migrationBuilder.Sql("DROP INDEX IF EXISTS [IX_BudgetPercentagePair_BudgetId] ON [BudgetPercentagePair]");

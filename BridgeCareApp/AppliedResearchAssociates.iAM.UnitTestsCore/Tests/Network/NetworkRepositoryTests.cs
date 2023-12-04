059f37b261f59510dbb12d87b64126d0e98ea8cd
William Jockusch <william.jockusch@sc-binc.com>
2023-12-04 15:12:41 -0600

    Wrote tests on NetworkRepository. They pass. But now seeing failures in SimulationOutputRepoTests. Why?

--- BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/Network/NetworkRepositoryTests.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/Network/NetworkRepositoryTests.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.Data;
+using AppliedResearchAssociates.iAM.Data.Networking;
 using AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories;
+using AppliedResearchAssociates.iAM.DataUnitTests.Tests;
+using AppliedResearchAssociates.iAM.TestHelpers;
+using AppliedResearchAssociates.iAM.UnitTestsCore.Tests.Repositories;
 using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
 using Xunit;
 
@@ -17,6 +22,45 @@ namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests
             var networkId = Guid.NewGuid();
             var networkNameOrId = TestHelper.UnitOfWork.NetworkRepo.GetNetworkNameOrId(networkId);
             Assert.Contains(networkNameOrId.ToString(), networkNameOrId);
+        }
+
+        [Fact]
+        public async Task DeleteNetwork_NetworkInDb_Deletes()
+        {
+            AttributeTestSetup.CreateAttributes(TestHelper.UnitOfWork);
+            var networkId = Guid.NewGuid();
+            var assetId = Guid.NewGuid();
+            var keyAttributeId = Guid.NewGuid();
+            var keyAttributeName = RandomStrings.WithPrefix("KeyAttribute");
+            var attributeDto = AttributeTestSetup.CreateSingleTextAttribute(TestHelper.UnitOfWork, keyAttributeId, keyAttributeName, ConnectionType.EXCEL, "location");
+            var asset = MaintainableAssets.InNetwork(networkId, keyAttributeName, assetId);
+            var assets = new List<MaintainableAsset> { asset };
+            var network = NetworkTestSetup.ModelForEntityInDb(TestHelper.UnitOfWork, assets, networkId, keyAttributeId);
+            var networksBefore = await TestHelper.UnitOfWork.NetworkRepo.Networks();
+            var networkBefore = networksBefore.Single(n => n.Id == networkId);
+
+            TestHelper.UnitOfWork.NetworkRepo.DeleteNetwork(networkId);
+
+            var networksAfter = await TestHelper.UnitOfWork.NetworkRepo.Networks();
+            var networkAfter = networksAfter.SingleOrDefault(n => n.Id == networkId);
+            Assert.Null(networkAfter);
+        }
+
+        [Fact]
+        public void GetNetworkName_NetworkInDb_Gets()
+        {
+            AttributeTestSetup.CreateAttributes(TestHelper.UnitOfWork);
+            var networkId = Guid.NewGuid();
+            var assetId = Guid.NewGuid();
+            var keyAttributeId = Guid.NewGuid();
+            var keyAttributeName = RandomStrings.WithPrefix("KeyAttribute");
+            var asset = MaintainableAssets.InNetwork(networkId, keyAttributeName, assetId);
+            var assets = new List<MaintainableAsset> { asset };
+            var network = NetworkTestSetup.ModelForEntityInDb(TestHelper.UnitOfWork, assets, networkId, keyAttributeId, "Network name");
+
+            var name = TestHelper.UnitOfWork.NetworkRepo.GetNetworkName(networkId);
+
+            Assert.Equal("Network name", name);
         }
     }
 }

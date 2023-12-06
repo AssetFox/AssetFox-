28a168a99216f1a993910e70a082119c34fa28f7
William Jockusch <william.jockusch@sc-binc.com>
2023-12-06 09:48:31 -0600

    Working on manually creating migration to drop BudgetId from CommittedProject. Not there yet.

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Migrations/20231206220510_RemoveColumnBudgetIdFromTableCommittedProject.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Migrations
+{
+    public partial class RemoveColumnBudgetIdFromTableCommittedProject
+        : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE [CommittedProject] DROP CONSTRAINT IF EXISTS [FK_BudgetPercentagePair_Budget_BudgetId];");
+            migrationBuilder.Sql("DROP INDEX IF EXISTS [IX_BudgetPercentagePair_BudgetId] ON [BudgetPercentagePair]");
+            migrationBuilder.Sql("ALTER TABLE [CommittedProject] DROP COLUMN IF EXISTS [BudgetId];");
+            migrationBuilder.Sql("ALTER TABLE [CommittedProject] WITH CHECK CHECK CONSTRAINT all;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

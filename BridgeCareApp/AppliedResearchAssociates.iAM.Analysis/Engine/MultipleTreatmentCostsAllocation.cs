eab1fb18b10006e23f3dcbe826f89a42d70c68fb
William Rummler <w.a.rummler@gmail.com>
2023-12-12 16:10:55 -0600

    WIP linear program for costs allocation.

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Analysis/Engine/MultipleTreatmentCostsAllocation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Google.OrTools.LinearSolver;
+
+namespace AppliedResearchAssociates.iAM.Analysis.Engine;
+
+public static class MultipleTreatmentCostsAllocation
+{
+    public static SolvedBudget[] Solve(
+        (int budget, int treatment)[] allowedSpending,
+        double[] budgetAmounts,
+        double[] treatmentCosts)
+    {
+        var solution = new SolvedBudget[budgetAmounts.Length];
+
+        for (var b_solving = 0;  b_solving < budgetAmounts.Length; ++b_solving)
+        {
+            var solver = Solver.CreateSolver("GLOP") ??
+                throw new Exception("Solver could not be created.");
+
+            for (var b_solved = 0; b_solved < b_solving; ++b_solved)
+            {
+                // vars
+            }
+
+            // b_solving vars
+
+            for (var b_unsolved = b_solving + 1; b_unsolved < budgetAmounts.Length; ++b_unsolved)
+            {
+                // vars
+            }
+        }
+    }
+
+    public sealed record SolvedBudget(double?[] Spent, double Unspent);
+}

80b18169f644593f6008087d2d9b797b79ff26b0
William Jockusch <william.jockusch@sc-binc.com>
2023-12-13 14:41:03 -0600

    This test runs the OrphanCleanup Sproc. Does not actually insert any orphans or check that they get cleaned up.

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/OrphanCleanup/OrphanCleanupSprocTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests
+{
+    public class OrphanCleanupSprocTests
+    {
+        private string RunOrphanCleanupSproc()
+        {
+            var retMessageParam = new SqlParameter("@RetMessage", SqlDbType.VarChar, 250);
+            retMessageParam.Direction = ParameterDirection.Output;
+            TestHelper.UnitOfWork.Context.Database.ExecuteSqlRaw("EXEC usp_orphan_cleanup @RetMessage", retMessageParam);
+            var retMessageValue = retMessageParam.Value;
+            return retMessageValue.ToString();
+        }
+
+        [Fact]
+        public void OrphanCleanup_Runs()
+        {
+            var retMessage = RunOrphanCleanupSproc();
+            Assert.Empty(retMessage);
+        }
+    }
+}

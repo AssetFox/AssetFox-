7422ed2961ec57a58d7ab8eddcb6685eb522b003
William Jockusch <william.jockusch@sc-binc.com>
2024-01-09 10:14:41 -0600

    Tests on TreatmentCostRepository. They caught a bug. Fixed it.

--- BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/ITreatmentCostRepository.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.DataPersistenceCore/Repositories/ITreatmentCostRepository.cs
@@ -7,9 +7,6 @@ namespace AppliedResearchAssociates.iAM.DataPersistenceCore.Repositories
 {
     public interface ITreatmentCostRepository
     {
-        void CreateScenarioTreatmentCosts(Dictionary<Guid, List<TreatmentCost>> treatmentCostsPerTreatmentId,
-            string simulationName);
-
         void UpsertOrDeleteTreatmentCosts(Dictionary<Guid, List<TreatmentCostDTO>> treatmentCostPerTreatmentId,
             Guid libraryId);
 
--- BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/TreatmentCost/TreatmentCostRepositoryTests.cs
+++ BridgeCareApp/AppliedResearchAssociates.iAM.UnitTestsCore/Tests/TreatmentCost/TreatmentCostRepositoryTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AppliedResearchAssociates.iAM.DTOs;
 using AppliedResearchAssociates.iAM.TestHelpers;
 using AppliedResearchAssociates.iAM.UnitTestsCore.TestUtils;
 using Xunit;
@@ -29,6 +30,48 @@ namespace AppliedResearchAssociates.iAM.UnitTestsCore.Tests.TreatmentCost
 
             var foundCost = costs.Single();
             ObjectAssertions.EquivalentExcluding(treatmentCost, foundCost, x => x.CriterionLibrary, x => x.Equation.Id);
+        }
+
+        [Fact]
+        public void UpsertOrDeleteTreatmentCosts_TreatmentInDbWithLibrary_Does()
+        {
+            var networkId = Guid.NewGuid();
+            var treatmentLibraryId = Guid.NewGuid();
+            var treatmentLibrary = TreatmentLibraryTestSetup.ModelForEntityInDb(TestHelper.UnitOfWork, treatmentLibraryId);
+            var treatmentId = Guid.NewGuid();
+            var treatment = TreatmentTestSetup.ModelForSingleTreatmentOfLibraryInDb(
+                TestHelper.UnitOfWork, treatmentLibraryId, treatmentId, "treatment");
+            var treatmentCost = TreatmentCostDtos.Dto();
+            var treatmentCosts = new List<TreatmentCostDTO> { treatmentCost };
+            var costDictionary = new Dictionary<Guid, List<TreatmentCostDTO>> { { treatmentId, treatmentCosts } };
+
+            TestHelper.UnitOfWork.TreatmentCostRepo.UpsertOrDeleteTreatmentCosts(costDictionary, treatmentLibraryId);
+
+            var costsAfter = TestHelper.UnitOfWork.TreatmentCostRepo.GetTreatmentCostByTreatmentId(treatmentId);
+            var costAfter = costsAfter.Single();
+            ObjectAssertions.EquivalentExcluding(treatmentCost, costAfter, c => c.Equation, c => c.CriterionLibrary);
+            ObjectAssertions.Equivalent(new EquationDTO(), costAfter.Equation);
+            ObjectAssertions.Equivalent(new CriterionLibraryDTO(), costAfter.CriterionLibrary);
+        }
+
+        [Fact]
+        public void UpsertOrDeleteTreatmentCosts_CostHasChildren_TreatmentInDbWithLibrary_Does()
+        {
+            var networkId = Guid.NewGuid();
+            var treatmentLibraryId = Guid.NewGuid();
+            var treatmentLibrary = TreatmentLibraryTestSetup.ModelForEntityInDb(TestHelper.UnitOfWork, treatmentLibraryId);
+            var treatmentId = Guid.NewGuid();
+            var treatment = TreatmentTestSetup.ModelForSingleTreatmentOfLibraryInDb(
+                TestHelper.UnitOfWork, treatmentLibraryId, treatmentId, "treatment");
+            var treatmentCost = TreatmentCostDtos.WithEquationAndCriterionLibrary();
+            var treatmentCosts = new List<TreatmentCostDTO> { treatmentCost };
+            var costDictionary = new Dictionary<Guid, List<TreatmentCostDTO>> { { treatmentId, treatmentCosts } };
+
+            TestHelper.UnitOfWork.TreatmentCostRepo.UpsertOrDeleteTreatmentCosts(costDictionary, treatmentLibraryId);
+
+            var costsAfter = TestHelper.UnitOfWork.TreatmentCostRepo.GetTreatmentCostByTreatmentId(treatmentId);
+            var costAfter = costsAfter.Single();
+            ObjectAssertions.EquivalentExcluding(treatmentCost, costAfter, tc => tc.CriterionLibrary.Name, tc => tc.CriterionLibrary.Id, tc => tc.Equation.Id);
         }
     }
 }

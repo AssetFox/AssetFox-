3c124c173e6b789e6b4e4732fba59e785d2774b1
William Rummler <w.a.rummler@gmail.com>
2024-01-25 12:39:14 -0600

    Un-remove BudgetUsageStatus.

---
+++ BridgeCareApp/AppliedResearchAssociates.iAM.Analysis/Engine/BudgetUsageStatus.cs
@@ -0,0 +1,38 @@
+﻿namespace AppliedResearchAssociates.iAM.Analysis.Engine;
+
+/// <summary>
+///     Represents the manner in which a budget was or was not used.
+/// </summary>
+public enum BudgetUsageStatus
+{
+    /// <summary>
+    ///     Indicates the existence of incomplete logic in the analysis engine.
+    /// </summary>
+    Undefined,
+
+    /// <summary>
+    ///     Indicates a budget that paid at least some of the cost of the treatment.
+    /// </summary>
+    CostCovered,
+
+    /// <summary>
+    ///     Indicates a budget that did not pay any cost of the treatment.
+    /// </summary>
+    CostNotCovered,
+
+    /// <summary>
+    ///     Indicates a budget with one or more user-defined conditions, none of which were met.
+    /// </summary>
+    ConditionNotMet,
+
+    /// <summary>
+    ///     Indicates a budget that was usable, but other budgets before this one in the scenario's
+    ///     budget order were sufficient to pay for the treatment.
+    /// </summary>
+    NotNeeded,
+
+    /// <summary>
+    ///     Indicates a budget excluded by the treatment's settings.
+    /// </summary>
+    NotUsable,
+}

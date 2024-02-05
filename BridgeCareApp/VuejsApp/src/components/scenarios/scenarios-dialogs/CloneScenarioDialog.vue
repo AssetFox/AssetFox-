32b80b97aef700f0f7b95682f8ae37a81361158d
Bryson Taylor <bryson.taylor@pertan.com>
2024-02-05 17:27:01 -0600

    getting rid of commented code

--- BridgeCareApp/VuejsApp/src/components/scenarios/scenarios-dialogs/CloneScenarioDialog.vue
+++ BridgeCareApp/VuejsApp/src/components/scenarios/scenarios-dialogs/CloneScenarioDialog.vue
@@ -163,7 +163,6 @@ import { useStore } from 'vuex';
             newScenario.networkId = props.dialogData.scenario.networkId
             newScenario.networkName = selectedNetworkName;      
             newScenario.name = props.dialogData.scenario.name;
-            // cloneScenarioWithDestinationNetworkAction(newScenario);
             emit('submit', newScenario);
         } else {
             emit('submit', null);

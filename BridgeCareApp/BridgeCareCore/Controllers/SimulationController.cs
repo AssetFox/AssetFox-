35bc5e7f71d9a975fabd504991fe69f0a442e188
Jaden Nobles <jnobles@ara.com>
2024-02-21 09:26:55 -0500

    Destination network check added back

--- BridgeCareApp/BridgeCareCore/Controllers/SimulationController.cs
+++ BridgeCareApp/BridgeCareCore/Controllers/SimulationController.cs
@@ -239,12 +239,15 @@ namespace BridgeCareCore.Controllers
                 var result = await Task.Factory.StartNew(() =>
                 {
                     _claimHelper.CheckUserSimulationModifyAuthorization(dto.ScenarioId, UserId);
-                    var isCompatible = _completeSimulationCloningService.CheckCompatibleNetworkAttributes(dto);
-                    if (!isCompatible)
+                    if (dto.DestinationNetworkId != dto.NetworkId)
                     {
-                        //Provide error message when networks are not compatible
-                        HubService.SendRealTimeMessage(UserInfo.Name, HubConstant.BroadcastError, $"{SimulationError}::CloneSimulation - destination network is not compatible.");
-                        return null;
+                        var isCompatible = _completeSimulationCloningService.CheckCompatibleNetworkAttributes(dto);
+                        if (!isCompatible)
+                        {
+                            //Provide error message when networks are not compatible
+                            HubService.SendRealTimeMessage(UserInfo.Name, HubConstant.BroadcastError, $"{SimulationError}::CloneSimulation - destination network is not compatible.");
+                            return null;
+                        }
                     }
                     var cloneResult = _completeSimulationCloningService.Clone(dto);
                     return cloneResult;
